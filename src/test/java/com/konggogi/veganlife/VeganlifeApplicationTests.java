afd3a58b79b2243b87fe841e508679f215c61114
sypark <soun997@gmail.com>
2023-11-23 00:28:13 +0900

    🎉initialize project

---
+++ src/test/java/com/konggogi/veganlife/VeganlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konggogi.veganlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeganlifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
