658d57cada14a9effe5bd0b6ddf310f7f835ed51
sojk401 <sojk401@gmail.com>
2023-12-06 11:17:45 +0900

    [#7] feat: 엔티티 생성 및 수정 시간 기록을 위한 TimeStamped 클래스 구현
    Related to: #7

--- src/main/java/com/konggogi/veganlife/VeganlifeApplication.java
+++ src/main/java/com/konggogi/veganlife/VeganlifeApplication.java
@@ -3,8 +3,10 @@ package com.konggogi.veganlife;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class VeganlifeApplication {
 
     public static void main(String[] args) {
