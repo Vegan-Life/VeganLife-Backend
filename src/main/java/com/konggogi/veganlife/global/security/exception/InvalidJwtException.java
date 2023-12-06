6bcdf3937d83d5f33dc2e314c6ef84350c807d37
sojk401 <sojk401@gmail.com>
2023-12-07 07:04:23 +0900

    [#10] feat: 커스텀 예외 InvalidJwtException 구현

---
+++ src/main/java/com/konggogi/veganlife/global/security/exception/InvalidJwtException.java
@@ -0,0 +1,11 @@
+package com.konggogi.veganlife.global.security.exception;
+
+
+import com.konggogi.veganlife.global.exception.ApiException;
+import com.konggogi.veganlife.global.exception.ErrorCode;
+
+public class InvalidJwtException extends ApiException {
+    public InvalidJwtException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
