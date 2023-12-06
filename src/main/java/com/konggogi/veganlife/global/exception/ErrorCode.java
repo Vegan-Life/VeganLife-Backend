cbfc6304f7ac4bc44f5428d84d6aad699e3f02f3
sojk401 <sojk401@gmail.com>
2023-12-07 03:30:51 +0900

    [#13] Refactor: ErrorCode 생성자 추가

--- src/main/java/com/konggogi/veganlife/global/exception/ErrorCode.java
+++ src/main/java/com/konggogi/veganlife/global/exception/ErrorCode.java
@@ -1,15 +1,18 @@
 package com.konggogi.veganlife.global.exception;
 
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public enum ErrorCode {
     // validation
     INVALID_INPUT_VALUE("GLOBAL_001", "RequestBody Validation에 실패했습니다.");
 
     private final String code;
     private final String description;
+
+    ErrorCode(String code, String description) {
+        this.code = code;
+        this.description = description;
+    }
 }
