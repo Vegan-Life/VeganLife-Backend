e8189cd57d58a50bac7d3138127a5bdaebd206cb
sojk401 <sojk401@gmail.com>
2023-12-08 06:34:26 +0900

    [#21] feat: 소셜 로그인 관련 OauthService 및 KakaoUserInfo 클래스 구현

---
+++ src/main/java/com/konggogi/veganlife/member/service/OauthService.java
@@ -0,0 +1,57 @@
+package com.konggogi.veganlife.member.service;
+
+
+import com.konggogi.veganlife.global.security.jwt.JwtProvider;
+import com.konggogi.veganlife.member.domain.KakaoUserInfo;
+import com.konggogi.veganlife.member.domain.Member;
+import com.konggogi.veganlife.member.domain.Role;
+import com.konggogi.veganlife.member.dto.response.OauthLoginResponse;
+import com.konggogi.veganlife.member.repository.MemberRepository;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class OauthService {
+    private final JwtProvider jwtProvider;
+    private final MemberRepository memberRepository;
+
+    public OauthLoginResponse loginWithToken(String token) {
+        Member member = createMemberFromToken(token);
+        String accessToken = jwtProvider.createToken(member.getEmail());
+        String refreshToken = jwtProvider.createRefreshToken(member.getEmail());
+        return createOauthLoginResponse(member, accessToken, refreshToken);
+    }
+
+    private OauthLoginResponse createOauthLoginResponse(
+            Member member, String accessToken, String refreshToken) {
+        return memberRepository
+                .findByEmail(member.getEmail())
+                .map(foundMember -> new OauthLoginResponse(true, accessToken, refreshToken))
+                .orElse(new OauthLoginResponse(false, accessToken, refreshToken));
+    }
+
+    private Member createMemberFromToken(String token) {
+        Map<String, Object> userAttributes = getUserAttributesByToken(token);
+        KakaoUserInfo kakaoUserInfo = new KakaoUserInfo(userAttributes);
+        // TODO gender, phoneNumber 추가
+        String email = kakaoUserInfo.getEmail();
+        String imageUrl = kakaoUserInfo.getProfileImageUrl();
+        return Member.builder().email(email).profileImageUrl(imageUrl).role(Role.USER).build();
+    }
+
+    private Map<String, Object> getUserAttributesByToken(String token) {
+        return WebClient.create()
+                .get()
+                .uri("https://kapi.kakao.com/v2/user/me")
+                .headers(httpHeaders -> httpHeaders.setBearerAuth(token))
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {})
+                .block();
+    }
+}
