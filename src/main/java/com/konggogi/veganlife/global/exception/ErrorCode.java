968e9e30a66ab5a4b9146bc421b6e408f7426450
sojk401 <sojk401@gmail.com>
2023-12-09 03:40:57 +0900

    [#21] feat: UnsupportedProviderException 구현 및 ErrorCode 정의

--- src/main/java/com/konggogi/veganlife/global/exception/ErrorCode.java
+++ src/main/java/com/konggogi/veganlife/global/exception/ErrorCode.java
@@ -14,7 +14,10 @@ public enum ErrorCode {
     INVALID_TOKEN_SIGNATURE("AUTH_003", "JWT 서명이 유효하지 않습니다."),
     UNSUPPORTED_TOKEN("AUTH_004", "지원되지 않는 JWT 토큰입니다."),
     UNEXPECTED_TOKEN("AUTH_005", "JWT 처리 중 예상치 못한 오류가 발생했습니다."),
-    NOT_FOUND_USER_INFO_TOKEN("AUTH_006", "JWT 사용자 정보를 가져올 수 없습니다.");
+    NOT_FOUND_USER_INFO_TOKEN("AUTH_006", "JWT 사용자 정보를 가져올 수 없습니다."),
+
+    // oauth
+    UNSUPPORTED_PROVIDER("OATUH_001", "지원되지 않는 provider입니다.");
 
     private final String code;
     private final String description;
