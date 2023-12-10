884b6d655a4c61b106eabfca6e1eeab8d58f3ace
sojk401 <sojk401@gmail.com>
2023-12-11 01:28:57 +0900

    [#40] feat: MemberController createMember 메서드 구현

---
+++ src/main/java/com/konggogi/veganlife/member/controller/MemberController.java
@@ -0,0 +1,34 @@
+package com.konggogi.veganlife.member.controller;
+
+
+import com.konggogi.veganlife.global.security.jwt.JwtProvider;
+import com.konggogi.veganlife.member.controller.dto.request.MemberRegisterRequest;
+import com.konggogi.veganlife.member.controller.dto.request.MemberRegisterResponse;
+import com.konggogi.veganlife.member.domain.Member;
+import com.konggogi.veganlife.member.domain.mapper.MemberMapper;
+import com.konggogi.veganlife.member.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/members")
+public class MemberController {
+    private final MemberService memberService;
+    private final MemberMapper memberMapper;
+    private final JwtProvider jwtProvider;
+
+    @PostMapping()
+    public ResponseEntity<MemberRegisterResponse> createMember(
+            @Valid @RequestBody MemberRegisterRequest memberRegisterRequest) {
+        Member member = memberService.addMember(memberRegisterRequest);
+        String accessToken = jwtProvider.createToken(member.getEmail());
+        String refreshToken = jwtProvider.createRefreshToken(member.getEmail());
+        return ResponseEntity.ok(memberMapper.from(member, accessToken, refreshToken));
+    }
+}
--- src/main/java/com/konggogi/veganlife/member/domain/mapper/MemberMapper.java
+++ src/main/java/com/konggogi/veganlife/member/domain/mapper/MemberMapper.java
@@ -2,6 +2,7 @@ package com.konggogi.veganlife.member.domain.mapper;
 
 
 import com.konggogi.veganlife.member.controller.dto.request.MemberRegisterRequest;
+import com.konggogi.veganlife.member.controller.dto.request.MemberRegisterResponse;
 import com.konggogi.veganlife.member.domain.Member;
 import com.konggogi.veganlife.member.domain.Role;
 import org.mapstruct.Mapper;
@@ -9,4 +10,6 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface MemberMapper {
     Member from(MemberRegisterRequest registerRequest, Role role);
+
+    MemberRegisterResponse from(Member member, String accessToken, String refreshToken);
 }
