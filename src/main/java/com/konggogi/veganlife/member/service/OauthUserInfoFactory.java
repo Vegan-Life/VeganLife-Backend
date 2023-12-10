0a03d2bcdd43544796526f928a91fcbd97424f46
sojk401 <sojk401@gmail.com>
2023-12-10 21:04:59 +0900

    [#29] refactor: createOauthUserInfo 메서드 switch문으로 변경

--- src/main/java/com/konggogi/veganlife/member/service/OauthUserInfoFactory.java
+++ src/main/java/com/konggogi/veganlife/member/service/OauthUserInfoFactory.java
@@ -1,10 +1,12 @@
 package com.konggogi.veganlife.member.service;
 
 
+import com.konggogi.veganlife.global.exception.ErrorCode;
 import com.konggogi.veganlife.member.domain.oauth.KakaoUserInfo;
 import com.konggogi.veganlife.member.domain.oauth.NaverUserInfo;
 import com.konggogi.veganlife.member.domain.oauth.OauthProvider;
 import com.konggogi.veganlife.member.domain.oauth.OauthUserInfo;
+import com.konggogi.veganlife.member.exception.UnsupportedProviderException;
 import java.util.Map;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +14,16 @@ import org.springframework.stereotype.Component;
 public class OauthUserInfoFactory {
     public OauthUserInfo createOauthUserInfo(
             OauthProvider provider, Map<String, Object> userAttributes) {
-        return provider == OauthProvider.KAKAO
-                ? new KakaoUserInfo(userAttributes)
-                : new NaverUserInfo(userAttributes);
+        switch (provider) {
+            case KAKAO -> {
+                return new KakaoUserInfo(userAttributes);
+            }
+            case NAVER -> {
+                return new NaverUserInfo(userAttributes);
+            }
+            default -> {
+                throw new UnsupportedProviderException(ErrorCode.UNSUPPORTED_PROVIDER);
+            }
+        }
     }
 }
