0259d3f63b72d117fbbe0f7c7bdf88d80ed30fd8
sojk401 <sojk401@gmail.com>
2023-12-10 19:32:08 +0900

    [#29] feat: createOauthUserInfo()에 provider가 naver인 경우 추가

--- src/main/java/com/konggogi/veganlife/member/service/OauthUserInfoFactory.java
+++ src/main/java/com/konggogi/veganlife/member/service/OauthUserInfoFactory.java
@@ -3,6 +3,7 @@ package com.konggogi.veganlife.member.service;
 
 import com.konggogi.veganlife.global.exception.ErrorCode;
 import com.konggogi.veganlife.member.domain.oauth.KakaoUserInfo;
+import com.konggogi.veganlife.member.domain.oauth.NaverUserInfo;
 import com.konggogi.veganlife.member.domain.oauth.OauthProvider;
 import com.konggogi.veganlife.member.domain.oauth.OauthUserInfo;
 import com.konggogi.veganlife.member.exception.UnsupportedProviderException;
@@ -15,6 +16,8 @@ public class OauthUserInfoFactory {
             OauthProvider provider, Map<String, Object> userAttributes) {
         if (OauthProvider.KAKAO == provider) {
             return new KakaoUserInfo(userAttributes);
+        } else if (OauthProvider.NAVER == provider) {
+            return new NaverUserInfo(userAttributes);
         } else {
             throw new UnsupportedProviderException(ErrorCode.UNSUPPORTED_PROVIDER);
         }
