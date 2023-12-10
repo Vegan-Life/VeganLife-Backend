8b23cfa9b0ee3f90a10da3f47e99d118964cae54
sojk401 <sojk401@gmail.com>
2023-12-10 20:02:31 +0900

    [#29] refactor: createOauthUserInfo 메서드 예외 처리 제거

--- src/main/java/com/konggogi/veganlife/member/service/OauthUserInfoFactory.java
+++ src/main/java/com/konggogi/veganlife/member/service/OauthUserInfoFactory.java
@@ -1,12 +1,10 @@
 package com.konggogi.veganlife.member.service;
 
 
-import com.konggogi.veganlife.global.exception.ErrorCode;
 import com.konggogi.veganlife.member.domain.oauth.KakaoUserInfo;
 import com.konggogi.veganlife.member.domain.oauth.NaverUserInfo;
 import com.konggogi.veganlife.member.domain.oauth.OauthProvider;
 import com.konggogi.veganlife.member.domain.oauth.OauthUserInfo;
-import com.konggogi.veganlife.member.exception.UnsupportedProviderException;
 import java.util.Map;
 import org.springframework.stereotype.Component;
 
@@ -14,12 +12,8 @@ import org.springframework.stereotype.Component;
 public class OauthUserInfoFactory {
     public OauthUserInfo createOauthUserInfo(
             OauthProvider provider, Map<String, Object> userAttributes) {
-        if (OauthProvider.KAKAO == provider) {
-            return new KakaoUserInfo(userAttributes);
-        } else if (OauthProvider.NAVER == provider) {
-            return new NaverUserInfo(userAttributes);
-        } else {
-            throw new UnsupportedProviderException(ErrorCode.UNSUPPORTED_PROVIDER);
-        }
+        return provider == OauthProvider.KAKAO
+                ? new KakaoUserInfo(userAttributes)
+                : new NaverUserInfo(userAttributes);
     }
 }
