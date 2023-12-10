3e250bb8690e12121ed11002d6e095b81b92b669
sojk401 <sojk401@gmail.com>
2023-12-11 06:23:17 +0900

    [#40] feat: Json 역직렬화를 위한 파싱 메서드 구현

--- src/main/java/com/konggogi/veganlife/member/domain/Gender.java
+++ src/main/java/com/konggogi/veganlife/member/domain/Gender.java
@@ -1,6 +1,18 @@
 package com.konggogi.veganlife.member.domain;
 
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.stream.Stream;
+
 public enum Gender {
     M,
-    F
+    F;
+
+    @JsonCreator
+    public static Gender parsing(String input) {
+        return Stream.of(Gender.values())
+                .filter(gender -> gender.name().equals(input.toUpperCase()))
+                .findFirst()
+                .orElse(null);
+    }
 }
