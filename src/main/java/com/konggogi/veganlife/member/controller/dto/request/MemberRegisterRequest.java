8cd945506f754fe1254f80e3ec43f25638055c0a
sojk401 <sojk401@gmail.com>
2023-12-11 06:24:08 +0900

    [#40] refactor: MemberRegisterRequest 필드에 validation 추가

--- src/main/java/com/konggogi/veganlife/member/controller/dto/request/MemberRegisterRequest.java
+++ src/main/java/com/konggogi/veganlife/member/controller/dto/request/MemberRegisterRequest.java
@@ -3,16 +3,19 @@ package com.konggogi.veganlife.member.controller.dto.request;
 
 import com.konggogi.veganlife.member.domain.Gender;
 import com.konggogi.veganlife.member.domain.VegetarianType;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import org.hibernate.validator.constraints.Length;
 
 public record MemberRegisterRequest(
-        @NotBlank String email,
-        @NotBlank String nickname,
+        @Email @NotBlank String email,
+        @NotBlank @Length(min = 2, max = 10) String nickname,
         @NotBlank String phoneNumber,
         @NotNull Gender gender,
         @NotNull VegetarianType vegetarianType,
-        @NotNull Integer birthYear,
-        @NotNull Integer age,
-        @NotNull Integer height,
-        @NotNull Integer weight) {}
+        @Positive @NotNull Integer birthYear,
+        @Positive @NotNull Integer age,
+        @Positive @NotNull Integer height,
+        @Positive @NotNull Integer weight) {}
