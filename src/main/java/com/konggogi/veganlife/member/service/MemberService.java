f8fc6ae6e5fb3421980cad9598efebc142a05ce7
sojk401 <sojk401@gmail.com>
2023-12-12 00:03:24 +0900

    [#64] refactor: addMember 메서드 미가입 회원일 경우 가입하도록 변경

--- src/main/java/com/konggogi/veganlife/member/service/MemberService.java
+++ src/main/java/com/konggogi/veganlife/member/service/MemberService.java
@@ -3,9 +3,7 @@ package com.konggogi.veganlife.member.service;
 
 import com.konggogi.veganlife.global.exception.ErrorCode;
 import com.konggogi.veganlife.global.exception.NotFoundEntityException;
-import com.konggogi.veganlife.member.controller.dto.request.MemberRegisterRequest;
 import com.konggogi.veganlife.member.domain.Member;
-import com.konggogi.veganlife.member.domain.mapper.MemberMapper;
 import com.konggogi.veganlife.member.exception.DuplicateNicknameException;
 import com.konggogi.veganlife.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,15 +15,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class MemberService {
     private final MemberRepository memberRepository;
-    private final MemberMapper memberMapper;
 
     @Transactional
-    public Member addMember(MemberRegisterRequest memberRegisterRequest) {
-        validateNickname(memberRegisterRequest.nickname());
-        Member member = memberMapper.toEntity(memberRegisterRequest);
-        Member savedMember = memberRepository.save(member);
-        savedMember.updateDailyIntake();
-        return savedMember;
+    public Member addMember(String email) {
+        return memberRepository
+                .findByEmail(email)
+                .orElseGet(
+                        () -> {
+                            Member member = Member.builder().email(email).build();
+                            return memberRepository.save(member);
+                        });
     }
 
     private void validateNickname(String nickname) {
