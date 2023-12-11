83b44363f675586170962fd8be2a77c25f4659dc
sojk401 <sojk401@gmail.com>
2023-12-12 00:04:16 +0900

    [#64] feat: saveRefreshToken 메서드 구현

--- src/main/java/com/konggogi/veganlife/member/service/MemberService.java
+++ src/main/java/com/konggogi/veganlife/member/service/MemberService.java
@@ -3,9 +3,11 @@ package com.konggogi.veganlife.member.service;
 
 import com.konggogi.veganlife.global.exception.ErrorCode;
 import com.konggogi.veganlife.global.exception.NotFoundEntityException;
+import com.konggogi.veganlife.global.security.jwt.RefreshToken;
 import com.konggogi.veganlife.member.domain.Member;
 import com.konggogi.veganlife.member.exception.DuplicateNicknameException;
 import com.konggogi.veganlife.member.repository.MemberRepository;
+import com.konggogi.veganlife.member.repository.RefreshTokenRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     @Transactional
     public Member addMember(String email) {
@@ -50,5 +53,17 @@ public class MemberService {
         return memberRepository
                 .findById(memberId)
                 .orElseThrow(() -> new NotFoundEntityException(ErrorCode.NOT_FOUND_MEMBER));
+    }
+
+    @Transactional
+    public void saveRefreshToken(Long memberId, String token) {
+        refreshTokenRepository
+                .findRefreshTokenByMemberId(memberId)
+                .ifPresentOrElse(
+                        refreshToken -> refreshToken.updateToken(token),
+                        () -> {
+                            RefreshToken newRefreshToken = new RefreshToken(token, memberId);
+                            refreshTokenRepository.save(newRefreshToken);
+                        });
     }
 }
