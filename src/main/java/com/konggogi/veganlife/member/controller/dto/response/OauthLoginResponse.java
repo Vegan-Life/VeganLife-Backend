1f1e858bbfda0497a755eca7c7dbf662ee113cc7
sojk401 <sojk401@gmail.com>
2023-12-11 21:05:49 +0900

    [#65] refactor: OauthLoginResponse 인자 수정

--- src/main/java/com/konggogi/veganlife/member/controller/dto/response/OauthLoginResponse.java
+++ src/main/java/com/konggogi/veganlife/member/controller/dto/response/OauthLoginResponse.java
@@ -1,3 +1,3 @@
 package com.konggogi.veganlife.member.controller.dto.response;
 
-public record OauthLoginResponse(String email, Integer birthYear, String phoneNumber) {}
+public record OauthLoginResponse(String email) {}
