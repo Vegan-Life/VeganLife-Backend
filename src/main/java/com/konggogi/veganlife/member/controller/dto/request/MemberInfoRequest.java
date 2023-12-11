9852fa0af0df99a9a7661a4a0a424a71c7a7617d
sojk401 <sojk401@gmail.com>
2023-12-12 00:53:37 +0900

    [#69] feat: 추가 정보 입력 요청 및 응답 dto 생성

---
+++ src/main/java/com/konggogi/veganlife/member/controller/dto/request/MemberInfoRequest.java
@@ -0,0 +1,17 @@
+package com.konggogi.veganlife.member.controller.dto.request;
+
+
+import com.konggogi.veganlife.member.domain.Gender;
+import com.konggogi.veganlife.member.domain.VegetarianType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import org.hibernate.validator.constraints.Length;
+
+public record MemberInfoRequest(
+        @NotBlank @Length(min = 2, max = 10) String nickname,
+        @NotNull Gender gender,
+        @NotNull VegetarianType vegetarianType,
+        @Positive @NotNull Integer birthYear,
+        @Positive @NotNull Integer height,
+        @Positive @NotNull Integer weight) {}
