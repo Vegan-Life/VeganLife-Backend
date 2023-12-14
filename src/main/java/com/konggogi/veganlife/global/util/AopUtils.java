4bc327207e5de1fe99b7ef69667b33c78681f32e
sypark <soun997@gmail.com>
2023-12-15 05:24:28 +0900

    [#89] feature: MethodHandler -> MethodSignature 변환 메서드 구현

--- src/main/java/com/konggogi/veganlife/global/util/AopUtils.java
+++ src/main/java/com/konggogi/veganlife/global/util/AopUtils.java
@@ -4,12 +4,19 @@ package com.konggogi.veganlife.global.util;
 import com.konggogi.veganlife.global.aop.domain.MethodSignature;
 import org.aspectj.lang.JoinPoint;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 
 @Component
 public class AopUtils {
     public static MethodSignature extractMethodSignature(JoinPoint joinPoint) {
         String className = joinPoint.getTarget().getClass().getSimpleName();
         String methodName = joinPoint.getSignature().getName();
+        return new MethodSignature(className, methodName);
+    }
+
+    public static MethodSignature extractMethodSignature(HandlerMethod handlerMethod) {
+        String className = handlerMethod.getClass().getSimpleName();
+        String methodName = handlerMethod.getMethod().getName();
         return new MethodSignature(className, methodName);
     }
 }
