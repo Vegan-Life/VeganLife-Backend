6816fb7112f80a7c81898b410df8e664d56d0541
sojk401 <sojk401@gmail.com>
2023-12-14 11:52:07 +0900

    [#64] style: OauthService 코드 포맷팅

--- src/main/java/com/konggogi/veganlife/member/service/OauthService.java
+++ src/main/java/com/konggogi/veganlife/member/service/OauthService.java
@@ -11,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
