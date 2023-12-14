8931c215eb71565b2a66d374e0d49bdb54a50d39
sypark <soun997@gmail.com>
2023-12-15 05:27:54 +0900

    [#89] feature: ErrorHandler에 로깅 로직 추가

--- src/main/java/com/konggogi/veganlife/global/advice/common/GlobalControllerAdvice.java
+++ src/main/java/com/konggogi/veganlife/global/advice/common/GlobalControllerAdvice.java
@@ -3,18 +3,22 @@ package com.konggogi.veganlife.global.advice.common;
 
 import com.konggogi.veganlife.global.exception.ErrorCode;
 import com.konggogi.veganlife.global.exception.dto.response.ErrorResponse;
+import com.konggogi.veganlife.global.util.AopUtils;
+import com.konggogi.veganlife.global.util.LoggingUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.HandlerMethod;
 
 @RestControllerAdvice
 public class GlobalControllerAdvice {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleValidationException(
-            MethodArgumentNotValidException exception) {
+            HandlerMethod handlerMethod, MethodArgumentNotValidException exception) {
         BindingResult bindingResult = exception.getBindingResult();
 
         StringBuilder errors = new StringBuilder();
@@ -23,9 +27,17 @@ public class GlobalControllerAdvice {
             errors.append(fieldError.getField());
             errors.append("](은)는 ");
             errors.append(fieldError.getDefaultMessage());
+            errors.append(" -> 입력된 값: ");
+            errors.append(fieldError.getRejectedValue());
             errors.append(". ");
         }
         errors.deleteCharAt(errors.length() - 1);
+
+        LoggingUtils.exceptionLog(
+                AopUtils.extractMethodSignature(handlerMethod),
+                HttpStatus.BAD_REQUEST,
+                exception,
+                errors.toString());
 
         ErrorResponse errorResponse =
                 ErrorResponse.from(ErrorCode.INVALID_INPUT_VALUE, errors.toString());
