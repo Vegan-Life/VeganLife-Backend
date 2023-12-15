e9aa7b52d1ac743b6c98aea8d2895174b73bb4bb
sypark <soun997@gmail.com>
2023-12-15 15:09:42 +0900

    [#88] test: 식품 데이터 상세 조회 repository 테스트

--- src/test/java/com/konggogi/veganlife/mealdata/repository/MealDataRepositoryTest.java
+++ src/test/java/com/konggogi/veganlife/mealdata/repository/MealDataRepositoryTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.konggogi.veganlife.mealdata.domain.MealData;
 import com.konggogi.veganlife.mealdata.fixture.MealDataFixture;
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,5 +35,19 @@ public class MealDataRepositoryTest {
         assertThat(result.getNumberOfElements()).isEqualTo(valid.size());
         assertThat(result.getContent().stream().map(MealData::getName))
                 .allMatch(s -> s.contains(keyword));
+    }
+
+    @Test
+    @DisplayName("ID에 해당하는 MealData 레코드 조회")
+    void findByIdTest() {
+        // given
+        MealData mealData = MealDataFixture.MEAL.get();
+        mealDataRepository.save(mealData);
+        // when
+        Optional<MealData> result1 = mealDataRepository.findById(mealData.getId());
+        Optional<MealData> result2 = mealDataRepository.findById(-999L);
+        // then
+        assertThat(result1.isEmpty()).isEqualTo(false);
+        assertThat(result2.isEmpty()).isEqualTo(true);
     }
 }
