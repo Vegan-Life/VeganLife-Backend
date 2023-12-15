6a21ca7a6b5e9f699a2d60c82bfc19a198d1334c
sypark <soun997@gmail.com>
2023-12-16 04:37:04 +0900

    [#95] feature: MealDataListResponse dto에 accessType 필드 추가

--- src/main/java/com/konggogi/veganlife/mealdata/controller/dto/response/MealDataListResponse.java
+++ src/main/java/com/konggogi/veganlife/mealdata/controller/dto/response/MealDataListResponse.java
@@ -1,3 +1,6 @@
 package com.konggogi.veganlife.mealdata.controller.dto.response;
 
-public record MealDataListResponse(Long id, String name) {}
+
+import com.konggogi.veganlife.mealdata.domain.AccessType;
+
+public record MealDataListResponse(Long id, String name, AccessType accessType) {}
