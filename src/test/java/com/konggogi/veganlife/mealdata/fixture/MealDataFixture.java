40aac238c75efbc38d2ce8bf555e37c7e9e4a8c6
sypark <soun997@gmail.com>
2023-12-15 15:07:06 +0900

    [#88] test: MealData Fixture 수정

--- src/test/java/com/konggogi/veganlife/mealdata/fixture/MealDataFixture.java
+++ src/test/java/com/konggogi/veganlife/mealdata/fixture/MealDataFixture.java
@@ -6,48 +6,18 @@ import com.konggogi.veganlife.mealdata.domain.MealData;
 import com.konggogi.veganlife.mealdata.domain.MealDataType;
 
 public enum MealDataFixture {
-    MEAL(
-            "디폴트 음식",
-            MealDataType.MEAL,
-            100,
-            100,
-            100D,
-            100D,
-            100D,
-            100D,
-            100D,
-            1D,
-            1D,
-            1D,
-            IntakeUnit.G),
-    PROCESSED(
-            "디폴트 가공식품",
-            MealDataType.PROCESSED,
-            100,
-            100,
-            100D,
-            100D,
-            100D,
-            100D,
-            100D,
-            1D,
-            1D,
-            1D,
-            IntakeUnit.G);
+    MEAL("디폴트 음식", MealDataType.MEAL, 100, 100, 10D, 1D, 1D, 1D, IntakeUnit.G),
+    PROCESSED("디폴트 가공식품", MealDataType.PROCESSED, 100, 100, 10D, 1D, 1D, 1D, IntakeUnit.G);
 
     private Long id = 1L;
     private String name;
     private MealDataType type;
     private Integer amount;
     private Integer amountPerServe;
-    private Double calorie;
-    private Double protein;
-    private Double fat;
-    private Double carbs;
-    private Double caloriePerGram;
-    private Double proteinPerGram;
-    private Double fatPerGram;
-    private Double carbsPerGram;
+    private Double caloriePerUnit;
+    private Double proteinPerUnit;
+    private Double fatPerUnit;
+    private Double carbsPerUnit;
     private IntakeUnit intakeUnit;
 
     MealDataFixture(
@@ -55,27 +25,19 @@ public enum MealDataFixture {
             MealDataType type,
             Integer amount,
             Integer amountPerServe,
-            Double calorie,
-            Double protein,
-            Double fat,
-            Double carbs,
-            Double caloriePerGram,
-            Double proteinPerGram,
-            Double fatPerGram,
-            Double carbsPerGram,
+            Double caloriePerUnit,
+            Double proteinPerUnit,
+            Double fatPerUnit,
+            Double carbsPerUnit,
             IntakeUnit intakeUnit) {
         this.name = name;
         this.type = type;
         this.amount = amount;
         this.amountPerServe = amountPerServe;
-        this.calorie = calorie;
-        this.protein = protein;
-        this.fat = fat;
-        this.carbs = carbs;
-        this.caloriePerGram = caloriePerGram;
-        this.proteinPerGram = proteinPerGram;
-        this.fatPerGram = fatPerGram;
-        this.carbsPerGram = carbsPerGram;
+        this.caloriePerUnit = caloriePerUnit;
+        this.proteinPerUnit = proteinPerUnit;
+        this.fatPerUnit = fatPerUnit;
+        this.carbsPerUnit = carbsPerUnit;
         this.intakeUnit = intakeUnit;
     }
 
@@ -87,14 +49,10 @@ public enum MealDataFixture {
                 type,
                 amount,
                 amountPerServe,
-                calorie,
-                protein,
-                fat,
-                carbs,
-                caloriePerGram,
-                proteinPerGram,
-                fatPerGram,
-                carbsPerGram,
+                caloriePerUnit,
+                proteinPerUnit,
+                fatPerUnit,
+                carbsPerUnit,
                 intakeUnit);
     }
 
@@ -106,33 +64,10 @@ public enum MealDataFixture {
                 type,
                 amount,
                 amountPerServe,
-                calorie,
-                protein,
-                fat,
-                carbs,
-                caloriePerGram,
-                proteinPerGram,
-                fatPerGram,
-                carbsPerGram,
-                intakeUnit);
-    }
-
-    public MealData getWithNameAndIntakeUnit(String name, IntakeUnit intakeUnit) {
-
-        return new MealData(
-                id++,
-                name,
-                type,
-                amount,
-                amountPerServe,
-                calorie,
-                protein,
-                fat,
-                carbs,
-                caloriePerGram,
-                proteinPerGram,
-                fatPerGram,
-                carbsPerGram,
+                caloriePerUnit,
+                proteinPerUnit,
+                fatPerUnit,
+                carbsPerUnit,
                 intakeUnit);
     }
 }
