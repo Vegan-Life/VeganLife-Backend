bc1a99ef69bbde0988ec77cc5a956a480f1ca2e9
sojk401 <sojk401@gmail.com>
2023-12-17 04:36:27 +0900

    [#106] refactor: 권장 섭취량 조회 응답 Dto 클래스명 변경

--- src/main/java/com/konggogi/veganlife/member/domain/mapper/MemberMapper.java
+++ src/main/java/com/konggogi/veganlife/member/domain/mapper/MemberMapper.java
@@ -1,10 +1,10 @@
 package com.konggogi.veganlife.member.domain.mapper;
 
 
-import com.konggogi.veganlife.member.controller.dto.response.DailyNutrientsResponse;
 import com.konggogi.veganlife.member.controller.dto.response.MemberInfoResponse;
 import com.konggogi.veganlife.member.controller.dto.response.MemberProfileResponse;
 import com.konggogi.veganlife.member.controller.dto.response.OauthLoginResponse;
+import com.konggogi.veganlife.member.controller.dto.response.RecommendNutrientsResponse;
 import com.konggogi.veganlife.member.domain.Member;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -19,5 +19,5 @@ public interface MemberMapper {
     MemberInfoResponse toMemberInfoResponse(Member member);
 
     @Mapping(target = "dailyCalorie", source = "member.AMR")
-    DailyNutrientsResponse toDailyNutrientsResponse(Member member);
+    RecommendNutrientsResponse toRecommendNutrientsResponse(Member member);
 }
