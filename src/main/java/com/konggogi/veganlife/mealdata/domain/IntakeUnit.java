78696cf266889b0ce1c77677b3eddfb67c0892e6
sypark <soun997@gmail.com>
2023-12-17 04:55:10 +0900

    [#107] feature: MealData 엔티티 수정

--- src/main/java/com/konggogi/veganlife/mealdata/domain/IntakeUnit.java
+++ src/main/java/com/konggogi/veganlife/mealdata/domain/IntakeUnit.java
@@ -1,16 +1,6 @@
 package com.konggogi.veganlife.mealdata.domain;
 
-
-import com.fasterxml.jackson.annotation.JsonCreator;
-
 public enum IntakeUnit {
-    G("g"),
-    ML("ml");
-
-    private final String value;
-
-    @JsonCreator
-    IntakeUnit(String value) {
-        this.value = value;
-    }
+    G,
+    ML;
 }
