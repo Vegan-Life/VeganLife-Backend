7529e10956d3e39f862f987fa1e88b7b5c775a43
sypark <soun997@gmail.com>
2023-12-17 04:56:21 +0900

    [#107] feature: 식품 정보 목록 조회, 상세 조회 API 수정

--- src/main/java/com/konggogi/veganlife/mealdata/controller/dto/response/MealDataListResponse.java
+++ src/main/java/com/konggogi/veganlife/mealdata/controller/dto/response/MealDataListResponse.java
@@ -1,6 +1,3 @@
 package com.konggogi.veganlife.mealdata.controller.dto.response;
 
-
-import com.konggogi.veganlife.mealdata.domain.AccessType;
-
-public record MealDataListResponse(Long id, String name, AccessType accessType) {}
+public record MealDataListResponse(Long id, String name) {}
