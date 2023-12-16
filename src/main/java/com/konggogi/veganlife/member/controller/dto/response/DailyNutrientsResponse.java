1b40d5c9170ccd5ea03d58538c5aa97e82c344d4
sojk401 <sojk401@gmail.com>
2023-12-17 03:26:37 +0900

    [#106] feat: DailyNutrientsResponse 생성

---
+++ src/main/java/com/konggogi/veganlife/member/controller/dto/response/DailyNutrientsResponse.java
@@ -0,0 +1,4 @@
+package com.konggogi.veganlife.member.controller.dto.response;
+
+public record DailyNutrientsResponse(
+        Integer dailyCalorie, Integer dailyCarbs, Integer dailyProtein, Integer dailyFat) {}
