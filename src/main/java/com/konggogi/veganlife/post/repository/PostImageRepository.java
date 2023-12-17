870c380301cebc37e89224f21aef94e11361fdd6
sojk401 <sojk401@gmail.com>
2023-12-18 04:43:12 +0900

    [#117] feat: Post, PostImage, Tag, PostTag JPA 레포지토리 생성

---
+++ src/main/java/com/konggogi/veganlife/post/repository/PostImageRepository.java
@@ -0,0 +1,7 @@
+package com.konggogi.veganlife.post.repository;
+
+
+import com.konggogi.veganlife.post.domain.PostImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostImageRepository extends JpaRepository<PostImage, Long> {}
---
+++ src/main/java/com/konggogi/veganlife/post/repository/TagRepository.java
@@ -0,0 +1,7 @@
+package com.konggogi.veganlife.post.repository;
+
+
+import com.konggogi.veganlife.post.domain.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {}
