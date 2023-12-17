7eb2d87f78b02b6b63a8e33846e9c896ed423f5d
sypark <soun997@gmail.com>
2023-12-18 03:11:31 +0900

    [#116] fix: 변경에 따른 mapper 클래스 수정

--- src/main/java/com/konggogi/veganlife/meallog/domain/mapper/MealLogMapper.java
+++ src/main/java/com/konggogi/veganlife/meallog/domain/mapper/MealLogMapper.java
@@ -1,13 +1,15 @@
 package com.konggogi.veganlife.meallog.domain.mapper;
 
 
-import com.konggogi.veganlife.meallog.controller.dto.request.MealLogAddRequest;
+import com.konggogi.veganlife.meallog.domain.Meal;
 import com.konggogi.veganlife.meallog.domain.MealLog;
+import com.konggogi.veganlife.meallog.domain.MealType;
 import com.konggogi.veganlife.member.domain.Member;
+import java.util.List;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface MealLogMapper {
 
-    MealLog mealLogAddRequestToEntity(MealLogAddRequest mealLogAddRequest, Member member);
+    MealLog toEntity(MealType mealType, List<Meal> meals, Member member);
 }
--- src/main/java/com/konggogi/veganlife/meallog/domain/mapper/MealMapper.java
+++ src/main/java/com/konggogi/veganlife/meallog/domain/mapper/MealMapper.java
@@ -4,7 +4,6 @@ package com.konggogi.veganlife.meallog.domain.mapper;
 import com.konggogi.veganlife.mealdata.domain.MealData;
 import com.konggogi.veganlife.meallog.controller.dto.request.MealAddRequest;
 import com.konggogi.veganlife.meallog.domain.Meal;
-import com.konggogi.veganlife.meallog.domain.MealLog;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -12,8 +11,7 @@ import org.mapstruct.Mapping;
 public interface MealMapper {
 
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "mealAddRequest.mealDataId", ignore = true)
     @Mapping(source = "mealAddRequest.name", target = "name")
     @Mapping(source = "mealAddRequest.intakeUnit", target = "intakeUnit")
-    Meal mealAddRequestToEntity(MealAddRequest mealAddRequest, MealLog mealLog, MealData mealData);
+    Meal mealAddRequestToEntity(MealAddRequest mealAddRequest, MealData mealData);
 }
