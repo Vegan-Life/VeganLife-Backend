b9bac585f0fcd295264cb5b4ae0b3b0bae13cc83
sypark <soun997@gmail.com>
2023-12-17 17:56:17 +0900

    [#94] fix: MealLogAddRequest에 누락도니 @Valid 어노테이션 추가

--- src/main/java/com/konggogi/veganlife/meallog/controller/dto/request/MealLogAddRequest.java
+++ src/main/java/com/konggogi/veganlife/meallog/controller/dto/request/MealLogAddRequest.java
@@ -2,9 +2,10 @@ package com.konggogi.veganlife.meallog.controller.dto.request;
 
 
 import com.konggogi.veganlife.meallog.domain.MealType;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.List;
 
 public record MealLogAddRequest(
-        @NotNull MealType mealType, @Size(min = 1) List<MealAddRequest> mealAddRequests) {}
+        @NotNull MealType mealType, @Valid @Size(min = 1) List<MealAddRequest> mealAddRequests) {}
