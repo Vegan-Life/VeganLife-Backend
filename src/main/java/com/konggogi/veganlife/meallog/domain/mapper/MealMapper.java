31b7f46cd15bad4378a45cdcb84518974eaebdfa
sypark <soun997@gmail.com>
2023-12-17 19:06:07 +0900

    [#94] fix: 불필요한 @Mapping 어노테이션 제거

--- src/main/java/com/konggogi/veganlife/meallog/domain/mapper/MealMapper.java
+++ src/main/java/com/konggogi/veganlife/meallog/domain/mapper/MealMapper.java
@@ -13,8 +13,6 @@ public interface MealMapper {
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "mealAddRequest.mealDataId", ignore = true)
-    @Mapping(source = "mealLog", target = "mealLog")
-    @Mapping(source = "mealData", target = "mealData")
     @Mapping(source = "mealAddRequest.name", target = "name")
     @Mapping(source = "mealAddRequest.intakeUnit", target = "intakeUnit")
     Meal mealAddRequestToEntity(MealAddRequest mealAddRequest, MealLog mealLog, MealData mealData);
