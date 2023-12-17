7e56fe97ef0fdb3a6f57b9e74b1469fde1e058e9
sypark <soun997@gmail.com>
2023-12-18 03:12:30 +0900

    [#116] fix: MealLog Service 로직 변경

--- src/main/java/com/konggogi/veganlife/meallog/service/MealLogService.java
+++ src/main/java/com/konggogi/veganlife/meallog/service/MealLogService.java
@@ -1,11 +1,14 @@
 package com.konggogi.veganlife.meallog.service;
 
 
+import com.konggogi.veganlife.mealdata.service.MealDataQueryService;
+import com.konggogi.veganlife.meallog.controller.dto.request.MealAddRequest;
 import com.konggogi.veganlife.meallog.controller.dto.request.MealLogAddRequest;
-import com.konggogi.veganlife.meallog.domain.MealLog;
+import com.konggogi.veganlife.meallog.domain.Meal;
 import com.konggogi.veganlife.meallog.domain.mapper.MealLogMapper;
+import com.konggogi.veganlife.meallog.domain.mapper.MealMapper;
 import com.konggogi.veganlife.meallog.repository.MealLogRepository;
-import com.konggogi.veganlife.member.domain.Member;
+import com.konggogi.veganlife.member.service.MemberQueryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,12 +18,24 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class MealLogService {
 
+    private final MemberQueryService memberQueryService;
+    private final MealDataQueryService mealDataQueryService;
     private final MealLogRepository mealLogRepository;
     private final MealLogMapper mealLogMapper;
+    private final MealMapper mealMapper;
 
-    public MealLog add(MealLogAddRequest mealLogAddRequest, Member member) {
+    public void add(MealLogAddRequest mealLogAddRequest, Long memberId) {
 
-        return mealLogRepository.save(
-                mealLogMapper.mealLogAddRequestToEntity(mealLogAddRequest, member));
+        mealLogRepository.save(
+                mealLogMapper.toEntity(
+                        mealLogAddRequest.mealType(),
+                        mealLogAddRequest.meals().stream().map(this::toMeal).toList(),
+                        memberQueryService.search(memberId)));
+    }
+
+    private Meal toMeal(MealAddRequest mealAddRequest) {
+
+        return mealMapper.mealAddRequestToEntity(
+                mealAddRequest, mealDataQueryService.search(mealAddRequest.mealDataId()));
     }
 }
