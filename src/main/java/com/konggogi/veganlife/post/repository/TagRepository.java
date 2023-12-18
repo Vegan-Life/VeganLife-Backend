5428269e55b597be0becb590fd04ee95fc06d53c
sojk401 <sojk401@gmail.com>
2023-12-18 22:55:28 +0900

    [#117] feat: findByName 메서드 추가

--- src/main/java/com/konggogi/veganlife/post/repository/TagRepository.java
+++ src/main/java/com/konggogi/veganlife/post/repository/TagRepository.java
@@ -2,6 +2,9 @@ package com.konggogi.veganlife.post.repository;
 
 
 import com.konggogi.veganlife.post.domain.Tag;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TagRepository extends JpaRepository<Tag, Long> {}
+public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findByName(String tagName);
+}
