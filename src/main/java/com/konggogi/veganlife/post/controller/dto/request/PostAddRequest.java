130c9b7665ecff8901d286eacae0754a575df0b8
sojk401 <sojk401@gmail.com>
2023-12-18 22:56:37 +0900

    [#117] feat: PostAddRequest Valid 추가, PostAddResponse 변경

--- src/main/java/com/konggogi/veganlife/post/controller/dto/request/PostAddRequest.java
+++ src/main/java/com/konggogi/veganlife/post/controller/dto/request/PostAddRequest.java
@@ -1,7 +1,13 @@
 package com.konggogi.veganlife.post.controller.dto.request;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import java.util.List;
+import org.hibernate.validator.constraints.Length;
 
 public record PostAddRequest(
-        String title, String content, List<String> imageUrls, List<String> tags) {}
+        @NotBlank @Length(min = 1, max = 20) String title,
+        @NotBlank @Length(min = 1, max = 1000) String content,
+        @Size(max = 5) List<String> imageUrls,
+        @Size(max = 5) List<String> tags) {}
--- src/main/java/com/konggogi/veganlife/post/controller/dto/response/PostAddResponse.java
+++ src/main/java/com/konggogi/veganlife/post/controller/dto/response/PostAddResponse.java
@@ -1,3 +1,6 @@
 package com.konggogi.veganlife.post.controller.dto.response;
 
-public record PostAddResponse(Long postId) {}
+
+import java.time.LocalDateTime;
+
+public record PostAddResponse(Long postId, LocalDateTime createdAt) {}
