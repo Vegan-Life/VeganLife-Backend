5bc39f6111630f186049af36d9b4950b0204a429
sojk401 <sojk401@gmail.com>
2023-12-19 01:12:43 +0900

    [#117] refactor: List<String>의 String 길이를 검증하는 애노테이션 적용

--- src/main/java/com/konggogi/veganlife/post/controller/dto/request/PostAddRequest.java
+++ src/main/java/com/konggogi/veganlife/post/controller/dto/request/PostAddRequest.java
@@ -1,6 +1,7 @@
 package com.konggogi.veganlife.post.controller.dto.request;
 
 
+import com.konggogi.veganlife.global.annotation.StringElementLength;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import java.util.List;
@@ -10,4 +11,4 @@ public record PostAddRequest(
         @NotBlank @Length(min = 1, max = 20) String title,
         @NotBlank @Length(min = 1, max = 1000) String content,
         @Size(max = 5) List<String> imageUrls,
-        @Size(max = 5) List<String> tags) {}
+        @Size(max = 5) @StringElementLength(max = 10) List<String> tags) {}
