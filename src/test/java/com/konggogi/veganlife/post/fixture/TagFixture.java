9b47600f3c6eaaf0348af5932d50bec11748701c
sojk401 <sojk401@gmail.com>
2023-12-19 02:51:27 +0900

    [#117] feat: TagFixture 구현

---
+++ src/test/java/com/konggogi/veganlife/post/fixture/TagFixture.java
@@ -0,0 +1,29 @@
+package com.konggogi.veganlife.post.fixture;
+
+
+import com.konggogi.veganlife.post.domain.Tag;
+
+public enum TagFixture {
+    DEFAULT("#태그");
+    private String name;
+
+    TagFixture(String name) {
+        this.name = name;
+    }
+
+    public Tag getTag() {
+        return Tag.builder().name(name).build();
+    }
+
+    public Tag getTagWithId(Long id) {
+        return Tag.builder().id(id).name(name).build();
+    }
+
+    public Tag getTagWithIdAndName(Long id, String name) {
+        return Tag.builder().id(id).name(name).build();
+    }
+
+    public Tag getTagWithName(String name) {
+        return Tag.builder().name(name).build();
+    }
+}
