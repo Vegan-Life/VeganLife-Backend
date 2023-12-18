c4cec3dc45b5356bc2ab85f7588346ba9c24cf33
sojk401 <sojk401@gmail.com>
2023-12-19 02:54:27 +0900

    [#117] feat: MapStructConfig postMapper 추가

--- src/test/java/com/konggogi/veganlife/config/MapStructConfig.java
+++ src/test/java/com/konggogi/veganlife/config/MapStructConfig.java
@@ -7,6 +7,8 @@ import com.konggogi.veganlife.member.domain.mapper.AuthMapper;
 import com.konggogi.veganlife.member.domain.mapper.AuthMapperImpl;
 import com.konggogi.veganlife.member.domain.mapper.MemberMapper;
 import com.konggogi.veganlife.member.domain.mapper.MemberMapperImpl;
+import com.konggogi.veganlife.post.domain.mapper.PostMapper;
+import com.konggogi.veganlife.post.domain.mapper.PostMapperImpl;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
@@ -26,5 +28,10 @@ public class MapStructConfig {
     @Bean
     public AuthMapper authMapper() {
         return new AuthMapperImpl();
+    }
+
+    @Bean
+    public PostMapper postMapper() {
+        return new PostMapperImpl();
     }
 }
