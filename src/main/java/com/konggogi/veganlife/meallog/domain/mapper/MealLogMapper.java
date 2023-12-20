fefa5d5185e5ff0a6f119ed19ea3240c1f8b5ffe
sypark <soun997@gmail.com>
2023-12-20 09:41:56 +0900

    [#116] fix: MealLogMapper 수정
    생성된 MealLogMapperImpl 클래스 toEntity() 메서드가 id를 임의로 초기화하는 오류 수정

--- src/main/java/com/konggogi/veganlife/meallog/domain/mapper/MealLogMapper.java
+++ src/main/java/com/konggogi/veganlife/meallog/domain/mapper/MealLogMapper.java
@@ -1,15 +1,15 @@
 package com.konggogi.veganlife.meallog.domain.mapper;
 
 
-import com.konggogi.veganlife.meallog.domain.Meal;
 import com.konggogi.veganlife.meallog.domain.MealLog;
 import com.konggogi.veganlife.meallog.domain.MealType;
 import com.konggogi.veganlife.member.domain.Member;
-import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface MealLogMapper {
 
-    MealLog toEntity(MealType mealType, List<Meal> meals, Member member);
+    @Mapping(target = "id", ignore = true)
+    MealLog toEntity(MealType mealType, Member member);
 }
