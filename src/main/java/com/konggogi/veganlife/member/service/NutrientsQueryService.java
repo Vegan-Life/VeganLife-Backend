439c937d8bb9de9f21662279ffb87fb65bb9cdac
sojk401 <sojk401@gmail.com>
2023-12-20 15:14:55 +0900

    [#109] feat: 해당 날짜에 작성한 MealLog 조회 및 Meal들의 영양소 정보를 합산하는 메서드 구현

---
+++ src/main/java/com/konggogi/veganlife/member/service/NutrientsQueryService.java
@@ -0,0 +1,51 @@
+package com.konggogi.veganlife.member.service;
+
+
+import com.konggogi.veganlife.meallog.domain.Meal;
+import com.konggogi.veganlife.meallog.domain.MealLog;
+import com.konggogi.veganlife.meallog.repository.MealLogRepository;
+import com.konggogi.veganlife.member.service.dto.IntakeNutrients;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Collection;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class NutrientsQueryService {
+    private final MemberQueryService memberQueryService;
+    private final MealLogRepository mealLogRepository;
+
+    public IntakeNutrients searchDailyIntakeNutrients(Long memberId, LocalDate date) {
+        memberQueryService.search(memberId);
+        LocalDateTime startDate = date.atStartOfDay();
+        LocalDateTime endDate = date.atTime(LocalTime.MAX);
+        List<Meal> meals =
+                mealLogRepository
+                        .findAllByMemberIdAndModifiedAtBetween(memberId, startDate, endDate)
+                        .stream()
+                        .map(MealLog::getMeals)
+                        .flatMap(Collection::stream)
+                        .toList();
+        return sumIntakeNutrients(meals);
+    }
+
+    private IntakeNutrients sumIntakeNutrients(List<Meal> meals) {
+        IntakeNutrients initIntakeNutrients = new IntakeNutrients(0, 0, 0, 0);
+        return meals.stream()
+                .reduce(
+                        initIntakeNutrients,
+                        (accumulator, meal) ->
+                                new IntakeNutrients(
+                                        accumulator.calorie() + meal.getCalorie(),
+                                        accumulator.carbs() + meal.getCarbs(),
+                                        accumulator.protein() + meal.getProtein(),
+                                        accumulator.fat() + meal.getFat()),
+                        IntakeNutrients::combine);
+    }
+}
