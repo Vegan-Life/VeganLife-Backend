528517bbb3889fb38f6dfee2e04dc19cdafb9626
sypark <soun997@gmail.com>
2023-12-20 09:43:59 +0900

    [#116] fix: MealLogService의 add() 메서드 수정
    MealLog와 Meal의 연관관계가 제대로 생성되도록 수정

--- src/main/java/com/konggogi/veganlife/meallog/service/MealLogService.java
+++ src/main/java/com/konggogi/veganlife/meallog/service/MealLogService.java
@@ -2,9 +2,8 @@ package com.konggogi.veganlife.meallog.service;
 
 
 import com.konggogi.veganlife.mealdata.service.MealDataQueryService;
-import com.konggogi.veganlife.meallog.controller.dto.request.MealAddRequest;
 import com.konggogi.veganlife.meallog.controller.dto.request.MealLogAddRequest;
-import com.konggogi.veganlife.meallog.domain.Meal;
+import com.konggogi.veganlife.meallog.domain.MealLog;
 import com.konggogi.veganlife.meallog.domain.mapper.MealLogMapper;
 import com.konggogi.veganlife.meallog.domain.mapper.MealMapper;
 import com.konggogi.veganlife.meallog.repository.MealLogRepository;
@@ -26,16 +25,15 @@ public class MealLogService {
 
     public void add(MealLogAddRequest mealLogAddRequest, Long memberId) {
 
-        mealLogRepository.save(
+        MealLog mealLog =
                 mealLogMapper.toEntity(
-                        mealLogAddRequest.mealType(),
-                        mealLogAddRequest.meals().stream().map(this::toMeal).toList(),
-                        memberQueryService.search(memberId)));
-    }
-
-    private Meal toMeal(MealAddRequest mealAddRequest) {
-
-        return mealMapper.mealAddRequestToEntity(
-                mealAddRequest, mealDataQueryService.search(mealAddRequest.mealDataId()));
+                        mealLogAddRequest.mealType(), memberQueryService.search(memberId));
+        mealLogAddRequest.meals().stream()
+                .map(
+                        request ->
+                                mealMapper.mealAddRequestToEntity(
+                                        request, mealDataQueryService.search(request.mealDataId())))
+                .forEach(mealLog::addMeal);
+        mealLogRepository.save(mealLog);
     }
 }
