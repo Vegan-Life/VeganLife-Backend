3399f03f940554187d03902db817964214a3f0c3
sojk401 <sojk401@gmail.com>
2023-12-20 15:41:33 +0900

    [#109] test: 리플렉션을 사용한 setModifiedAt 메서드 생성

--- src/test/java/com/konggogi/veganlife/meallog/fixture/MealLogFixture.java
+++ src/test/java/com/konggogi/veganlife/meallog/fixture/MealLogFixture.java
@@ -5,7 +5,10 @@ import com.konggogi.veganlife.meallog.domain.Meal;
 import com.konggogi.veganlife.meallog.domain.MealLog;
 import com.konggogi.veganlife.meallog.domain.MealType;
 import com.konggogi.veganlife.member.domain.Member;
+import java.lang.reflect.Field;
+import java.time.LocalDateTime;
 import java.util.List;
+import org.springframework.util.ReflectionUtils;
 
 public enum MealLogFixture {
     BREAKFAST(MealType.BREAKFAST),
@@ -25,13 +28,20 @@ public enum MealLogFixture {
 
         MealLog mealLog = MealLog.builder().mealType(mealType).member(member).build();
         meals.forEach(mealLog::addMeal);
-        return mealLog;
+        return setModifiedAt(mealLog);
     }
 
     public MealLog getWithId(Long id, List<Meal> meals, Member member) {
 
         MealLog mealLog = MealLog.builder().id(id).mealType(mealType).member(member).build();
         meals.forEach(mealLog::addMeal);
+        return setModifiedAt(mealLog);
+    }
+
+    private MealLog setModifiedAt(MealLog mealLog) {
+        Field modifiedAt = ReflectionUtils.findField(MealLog.class, "modifiedAt");
+        ReflectionUtils.makeAccessible(modifiedAt);
+        ReflectionUtils.setField(modifiedAt, mealLog, LocalDateTime.now());
         return mealLog;
     }
 }
