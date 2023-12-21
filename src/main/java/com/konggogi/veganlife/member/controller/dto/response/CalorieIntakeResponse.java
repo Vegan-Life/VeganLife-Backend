ccc79954d36316ddc625b7072f294d02f94684a8
sojk401 <sojk401@gmail.com>
2023-12-22 04:41:31 +0900

    [#127] feat: CalorieIntakeResponse 생성

---
+++ src/main/java/com/konggogi/veganlife/member/controller/dto/response/CalorieIntakeResponse.java
@@ -0,0 +1,7 @@
+package com.konggogi.veganlife.member.controller.dto.response;
+
+
+import com.konggogi.veganlife.member.service.dto.CaloriesOfMealType;
+import java.util.List;
+
+public record CalorieIntakeResponse(Integer totalCalorie, List<CaloriesOfMealType> weekly) {}
