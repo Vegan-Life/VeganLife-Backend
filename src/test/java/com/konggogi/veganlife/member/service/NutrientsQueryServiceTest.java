76580bf20b4e231b82c656edb8edb4ff1a611e14
sojk401 <sojk401@gmail.com>
2023-12-22 06:17:30 +0900

    [#127] test: 주간 섭취량 조회, 총 칼로리 계산 테스트

--- src/test/java/com/konggogi/veganlife/member/service/NutrientsQueryServiceTest.java
+++ src/test/java/com/konggogi/veganlife/member/service/NutrientsQueryServiceTest.java
@@ -2,6 +2,8 @@ package com.konggogi.veganlife.member.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 
 import com.konggogi.veganlife.global.exception.ErrorCode;
@@ -14,7 +16,9 @@ import com.konggogi.veganlife.meallog.fixture.MealFixture;
 import com.konggogi.veganlife.meallog.fixture.MealLogFixture;
 import com.konggogi.veganlife.meallog.repository.MealLogRepository;
 import com.konggogi.veganlife.member.domain.Member;
+import com.konggogi.veganlife.member.fixture.CaloriesOfMealTypeFixture;
 import com.konggogi.veganlife.member.fixture.MemberFixture;
+import com.konggogi.veganlife.member.service.dto.CaloriesOfMealType;
 import com.konggogi.veganlife.member.service.dto.IntakeNutrients;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -80,5 +84,76 @@ class NutrientsQueryServiceTest {
                                         memberId, LocalDate.now()))
                 .isInstanceOf(NotFoundEntityException.class)
                 .hasMessageContaining(ErrorCode.NOT_FOUND_MEMBER.getDescription());
+    }
+
+    @Test
+    @DisplayName("주간 섭취량 조회")
+    void searchWeeklyIntakeCalorieTest() {
+        // given
+        Long memberId = member.getId();
+        Meal meal = meals.get(0);
+        LocalDate startDate = LocalDate.of(2023, 12, 17);
+        LocalDate endDate = LocalDate.of(2023, 12, 23);
+        List<MealLog> mealLogs = createMealLogs(startDate);
+        given(memberQueryService.search(memberId)).willReturn(member);
+        given(
+                        mealLogRepository.findAllByMemberIdAndModifiedAtBetween(
+                                anyLong(), any(LocalDateTime.class), any(LocalDateTime.class)))
+                .willReturn(mealLogs);
+        // when
+        List<CaloriesOfMealType> caloriesOfMealTypes =
+                nutrientsQueryService.searchWeeklyIntakeCalories(memberId, startDate, endDate);
+        // then
+        assertThat(caloriesOfMealTypes).hasSize(7);
+        assertThat(caloriesOfMealTypes.get(0).breakfast())
+                .isEqualTo(meal.getCalorie() * meals.size());
+        assertThat(caloriesOfMealTypes.get(0).lunch()).isEqualTo(meal.getCalorie() * meals.size());
+        assertThat(caloriesOfMealTypes.get(0).dinner()).isEqualTo(meal.getCalorie() * meals.size());
+        assertThat(caloriesOfMealTypes.get(0).snack())
+                .isEqualTo(meal.getCalorie() * meals.size() * 3);
+    }
+
+    @Test
+    @DisplayName("주간 섭취량 조회시 없는 회원이면 예외 발생")
+    void searchWeeklyIntakeNutrientsNotMemberTest() {
+        // given
+        Long memberId = member.getId();
+        given(memberQueryService.search(memberId))
+                .willThrow(new NotFoundEntityException(ErrorCode.NOT_FOUND_MEMBER));
+        // when, then
+        assertThatThrownBy(
+                        () ->
+                                nutrientsQueryService.searchWeeklyIntakeCalories(
+                                        memberId, LocalDate.now(), LocalDate.now()))
+                .isInstanceOf(NotFoundEntityException.class)
+                .hasMessageContaining(ErrorCode.NOT_FOUND_MEMBER.getDescription());
+    }
+
+    @Test
+    @DisplayName("총 섭취한 칼로리 계산")
+    void calcTotalCalorieTest() {
+        // given
+        List<CaloriesOfMealType> caloriesOfMealTypes =
+                List.of(
+                        CaloriesOfMealTypeFixture.DEFAULT.get(),
+                        CaloriesOfMealTypeFixture.DEFAULT.get());
+        int expectedCalorie =
+                CaloriesOfMealTypeFixture.DEFAULT.get().breakfast()
+                        * 4
+                        * caloriesOfMealTypes.size();
+        // when
+        int totalCalorie = nutrientsQueryService.calcTotalCalorie(caloriesOfMealTypes);
+        // then
+        assertThat(totalCalorie).isEqualTo(expectedCalorie);
+    }
+
+    private List<MealLog> createMealLogs(LocalDate date) {
+        return List.of(
+                MealLogFixture.BREAKFAST.getWithDate(meals, member, date),
+                MealLogFixture.LUNCH.getWithDate(meals, member, date),
+                MealLogFixture.DINNER.getWithDate(meals, member, date),
+                MealLogFixture.BREAKFAST_SNACK.getWithDate(meals, member, date),
+                MealLogFixture.LUNCH_SNACK.getWithDate(meals, member, date),
+                MealLogFixture.DINNER_SNACK.getWithDate(meals, member, date));
     }
 }
