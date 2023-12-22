b392d05f89c4eaf0f2762f91e7d6c9b03432cf94
sypark <soun997@gmail.com>
2023-12-22 21:10:28 +0900

    [#124] feat: MealLogList service dto 구현

---
+++ src/main/java/com/konggogi/veganlife/meallog/service/dto/MealLogList.java
@@ -0,0 +1,6 @@
+package com.konggogi.veganlife.meallog.service.dto;
+
+
+import com.konggogi.veganlife.meallog.domain.MealType;
+
+public record MealLogList(MealType mealType, String imageUrl, Integer totalCalorie) {}
