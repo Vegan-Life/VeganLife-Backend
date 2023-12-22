bbee7bcf41d3bc3342d16b0d5e2005bdd5bbeb68
sypark <soun997@gmail.com>
2023-12-22 17:56:37 +0900

    [#136] feat: MealImage 엔티티 구현 및 연관관계 설정

---
+++ src/main/java/com/konggogi/veganlife/meallog/domain/MealImage.java
@@ -0,0 +1,42 @@
+package com.konggogi.veganlife.meallog.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MealImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "meal_log_id")
+    private MealLog mealLog;
+
+    @Builder
+    public MealImage(Long id, String imageUrl, MealLog mealLog) {
+        this.id = id;
+        this.imageUrl = imageUrl;
+        this.mealLog = mealLog;
+    }
+
+    public void setMealLog(MealLog mealLog) {
+        this.mealLog = mealLog;
+    }
+}
