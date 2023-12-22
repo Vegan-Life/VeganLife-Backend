6a170ef2219cfbd903cd741bb6aed7fa92fde3b4
sypark <soun997@gmail.com>
2023-12-22 17:58:16 +0900

    [#136] feat: MealImage repository 클래스 구현

---
+++ src/main/java/com/konggogi/veganlife/meallog/repository/MealImageRepository.java
@@ -0,0 +1,7 @@
+package com.konggogi.veganlife.meallog.repository;
+
+
+import com.konggogi.veganlife.meallog.domain.MealImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MealImageRepository extends JpaRepository<MealImage, Long> {}
