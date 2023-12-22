cc7fa480f86857025e6349ccc2c0d0a73ae9dda8
sypark <soun997@gmail.com>
2023-12-22 21:10:06 +0900

    [#124] feat: 해당 날짜의 MealLog 조회 쿼리 구현

--- src/main/java/com/konggogi/veganlife/meallog/repository/MealLogRepository.java
+++ src/main/java/com/konggogi/veganlife/meallog/repository/MealLogRepository.java
@@ -2,13 +2,19 @@ package com.konggogi.veganlife.meallog.repository;
 
 
 import com.konggogi.veganlife.meallog.domain.MealLog;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MealLogRepository extends JpaRepository<MealLog, Long> {
     List<MealLog> findAllByMemberIdAndModifiedAtBetween(
             Long memberId, LocalDateTime startDate, LocalDateTime endDate);
+
+    @Query(
+            "select m from MealLog m where cast(m.createdAt as localdate) = :date and m.member.id = :memberId")
+    List<MealLog> findAllByDate(LocalDate date, Long memberId);
 }
