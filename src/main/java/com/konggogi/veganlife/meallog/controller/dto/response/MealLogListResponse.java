058bb7202d3b3bff9061b77e22c0ecdc8e077885
sypark <soun997@gmail.com>
2023-12-22 21:18:14 +0900

    [#124] fix: response dto에 누락된 id 필드 추가

--- src/main/java/com/konggogi/veganlife/meallog/controller/dto/response/MealLogListResponse.java
+++ src/main/java/com/konggogi/veganlife/meallog/controller/dto/response/MealLogListResponse.java
@@ -3,4 +3,5 @@ package com.konggogi.veganlife.meallog.controller.dto.response;
 
 import com.konggogi.veganlife.meallog.domain.MealType;
 
-public record MealLogListResponse(MealType mealType, String thumbnailUrl, Integer totalCalorie) {}
+public record MealLogListResponse(
+        Long id, MealType mealType, String thumbnailUrl, Integer totalCalorie) {}
