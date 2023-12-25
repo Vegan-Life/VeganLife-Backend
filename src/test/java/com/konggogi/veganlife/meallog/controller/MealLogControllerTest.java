8a9e990054a5d581bfc26f4c0b055b7187fa05bc
sypark <soun997@gmail.com>
2023-12-25 10:54:52 +0900

    [#135] test: MealLog 상세 조회 controller 단위 테스트

--- src/test/java/com/konggogi/veganlife/meallog/controller/MealLogControllerTest.java
+++ src/test/java/com/konggogi/veganlife/meallog/controller/MealLogControllerTest.java
@@ -10,6 +10,7 @@ import static org.springframework.restdocs.headers.HeaderDocumentation.requestHe
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -34,8 +35,10 @@ import com.konggogi.veganlife.meallog.fixture.MealImageFixture;
 import com.konggogi.veganlife.meallog.fixture.MealLogFixture;
 import com.konggogi.veganlife.meallog.service.MealLogQueryService;
 import com.konggogi.veganlife.meallog.service.MealLogService;
+import com.konggogi.veganlife.meallog.service.dto.MealLogDetails;
 import com.konggogi.veganlife.meallog.service.dto.MealLogList;
 import com.konggogi.veganlife.member.domain.Member;
+import com.konggogi.veganlife.member.service.dto.IntakeNutrients;
 import com.konggogi.veganlife.support.docs.RestDocsTest;
 import java.time.LocalDate;
 import java.util.List;
@@ -60,7 +63,7 @@ public class MealLogControllerTest extends RestDocsTest {
     List<MealData> mealData =
             List.of(
                     MealDataFixture.MEAL.get(member),
-                    MealDataFixture.MEAL.get(member),
+                    MealDataFixture.PROCESSED.get(member),
                     MealDataFixture.MEAL.get(member));
 
     List<MealAddRequest> mealAddRequests =
@@ -189,6 +192,57 @@ public class MealLogControllerTest extends RestDocsTest {
                                 queryParameters(parameterWithName("date").description("조회 날짜"))));
     }
 
+    @Test
+    @DisplayName("식사 기록 상세 조회 API")
+    void getMealLogDetailsTest() throws Exception {
+
+        MealLogDetails mealLogDetails = getMealLogDetails();
+
+        given(mealLogQueryService.search(1L)).willReturn(mealLogDetails);
+
+        ResultActions perform =
+                mockMvc.perform(get("/api/v1/meal-log/{id}", 1L).headers(authorizationHeader()));
+
+        perform.andExpect(status().isOk())
+                .andExpect(jsonPath("$.mealLogId").value(1))
+                .andExpect(jsonPath("$.mealType").value(MealType.BREAKFAST.name()))
+                .andExpect(jsonPath("$.intakeNutrients.calorie").value(300))
+                .andExpect(jsonPath("$.intakeNutrients.carbs").value(30))
+                .andExpect(jsonPath("$.intakeNutrients.protein").value(30))
+                .andExpect(jsonPath("$.intakeNutrients.fat").value(30))
+                .andExpect(jsonPath("$.imageUrls.size()").value(3))
+                .andExpect(jsonPath("$.meals.size()").value(3));
+
+        perform.andDo(print())
+                .andDo(
+                        document(
+                                "meal-log-get-meal-details",
+                                getDocumentRequest(),
+                                getDocumentResponse(),
+                                requestHeaders(authorizationDesc()),
+                                pathParameters(parameterWithName("id").description("식단 기록 id"))));
+    }
+
+    @Test
+    @DisplayName("식사 기록 상세 조회 API - MealLog Not Found")
+    void getMealLogDetailsNotFoundTest() throws Exception {
+
+        given(mealLogQueryService.search(1L))
+                .willThrow(new NotFoundEntityException(ErrorCode.NOT_FOUND_MEAL_LOG));
+
+        ResultActions perform =
+                mockMvc.perform(get("/api/v1/meal-log/{id}", 1L).headers(authorizationHeader()));
+
+        perform.andExpect(status().isNotFound());
+
+        perform.andDo(print())
+                .andDo(
+                        document(
+                                "meal-log-get-meal-details-not-found",
+                                getDocumentResponse(),
+                                requestHeaders(authorizationDesc())));
+    }
+
     private MealLogList getMealLogList(long v) {
         List<Meal> meals =
                 LongStream.range(v, v + 3)
@@ -205,5 +259,25 @@ public class MealLogControllerTest extends RestDocsTest {
                 MealLogFixture.BREAKFAST.getWithDate(
                         v + 1, LocalDate.of(2022, 12, 22), meals, mealImages, member);
         return new MealLogList(mealLog, "image" + (v + 1) + ".png", 300);
+    }
+
+    private MealLogDetails getMealLogDetails() {
+        List<Meal> meals =
+                List.of(
+                        MealFixture.DEFAULT.get(1L, mealData.get(0)),
+                        MealFixture.DEFAULT.get(2L, mealData.get(1)),
+                        MealFixture.DEFAULT.get(3L, mealData.get(2)));
+        IntakeNutrients intakeNutrients =
+                new IntakeNutrients(
+                        meals.stream().mapToInt(Meal::getCalorie).sum(),
+                        meals.stream().mapToInt(Meal::getCarbs).sum(),
+                        meals.stream().mapToInt(Meal::getProtein).sum(),
+                        meals.stream().mapToInt(Meal::getFat).sum());
+        List<MealImage> mealImages =
+                LongStream.range(1, 4).mapToObj(MealImageFixture.DEFAULT::get).toList();
+        MealLog mealLog =
+                MealLogFixture.BREAKFAST.getWithDate(
+                        1L, LocalDate.of(2023, 12, 22), meals, mealImages, member);
+        return new MealLogDetails(mealLog, intakeNutrients, mealImages, meals);
     }
 }
