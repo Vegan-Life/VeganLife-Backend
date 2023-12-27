01ead4394a303326a0eeaa1e85a0383d45b4cb27
sypark <soun997@gmail.com>
2023-12-28 08:56:39 +0900

    [#137] fix: MealData 키워드 조회 기능 수정 -> 조건에 OwnerType 포함

--- src/main/java/com/konggogi/veganlife/mealdata/repository/MealDataRepository.java
+++ src/main/java/com/konggogi/veganlife/mealdata/repository/MealDataRepository.java
@@ -2,7 +2,8 @@ package com.konggogi.veganlife.mealdata.repository;
 
 
 import com.konggogi.veganlife.mealdata.domain.MealData;
-import java.util.List;
+import com.konggogi.veganlife.mealdata.domain.OwnerType;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +11,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MealDataRepository extends JpaRepository<MealData, Long> {
 
-    List<MealData> findByNameContaining(String Keyword, Pageable pageable);
+    Page<MealData> findByNameContainingAndOwnerType(
+            String keyword, OwnerType ownerType, Pageable pageable);
 }
