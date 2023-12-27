88f591b2208b402b6ff4bbc0e2e368593c5006e8
sypark <soun997@gmail.com>
2023-12-28 02:52:55 +0900

    [#145] feat: MealData 등록 API 구현

--- src/main/java/com/konggogi/veganlife/mealdata/controller/MealDataController.java
+++ src/main/java/com/konggogi/veganlife/mealdata/controller/MealDataController.java
@@ -1,16 +1,24 @@
 package com.konggogi.veganlife.mealdata.controller;
 
 
+import com.konggogi.veganlife.global.security.user.UserDetailsImpl;
+import com.konggogi.veganlife.mealdata.controller.dto.request.MealDataAddRequest;
 import com.konggogi.veganlife.mealdata.controller.dto.response.MealDataDetailsResponse;
 import com.konggogi.veganlife.mealdata.controller.dto.response.MealDataListResponse;
 import com.konggogi.veganlife.mealdata.domain.mapper.MealDataMapper;
 import com.konggogi.veganlife.mealdata.service.MealDataQueryService;
+import com.konggogi.veganlife.mealdata.service.MealDataService;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MealDataController {
 
     private final MealDataQueryService mealDataQueryService;
+    private final MealDataService mealDataService;
     private final MealDataMapper mealDataMapper;
 
     @GetMapping
@@ -37,5 +46,14 @@ public class MealDataController {
 
         return ResponseEntity.ok(
                 mealDataMapper.toMealDataDetailsResponse(mealDataQueryService.search(id)));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> addMealData(
+            @Valid @RequestBody MealDataAddRequest request,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        mealDataService.add(request, userDetails.id());
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
