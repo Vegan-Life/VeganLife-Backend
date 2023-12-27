0769242bd4c59c1a268c87310f8662e5b27d4771
sojk401 <sojk401@gmail.com>
2023-12-28 00:11:47 +0900

    [#164] feat: sse 구독 API 구현

---
+++ src/main/java/com/konggogi/veganlife/sse/controller/SseController.java
@@ -0,0 +1,25 @@
+package com.konggogi.veganlife.sse.controller;
+
+
+import com.konggogi.veganlife.global.security.user.UserDetailsImpl;
+import com.konggogi.veganlife.sse.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/sse")
+public class SseController {
+    private final NotificationService notificationService;
+
+    @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public ResponseEntity<SseEmitter> subscribe(
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        SseEmitter emitter = notificationService.subscribe(userDetails.id());
+        return ResponseEntity.ok(emitter);
+    }
+}
