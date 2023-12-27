2e0a6ed05636c6a9b24bb941f0c3987b4b74af56
sojk401 <sojk401@gmail.com>
2023-12-28 02:44:01 +0900

    [#164] feat: OVER_INTAKE 추가 및 setCalorie() 생성

--- src/main/java/com/konggogi/veganlife/sse/domain/NotificationMessage.java
+++ src/main/java/com/konggogi/veganlife/sse/domain/NotificationMessage.java
@@ -6,11 +6,21 @@ import lombok.Getter;
 @Getter
 public enum NotificationMessage {
     // sse
-    SSE_CONNECTION("SSE 연결이 완료되었습니다.");
+    SSE_CONNECTION("SSE 연결이 완료되었습니다."),
+
+    // intake
+    OVER_INTAKE("오늘의 칼로리 섭취량이 %dKcal 초과되었습니다.");
 
     private final String message;
 
     NotificationMessage(String message) {
         this.message = message;
+    }
+
+    public String setCalorie(int calorie) {
+        if (this == OVER_INTAKE) {
+            return String.format(this.message, calorie);
+        }
+        return this.message;
     }
 }
