6232dec7986721ae2ff1e410c983caf71a403765
sojk401 <sojk401@gmail.com>
2023-12-28 04:06:50 +0900

    [#164] feat: 알림 전송 시 NotificationData를 사용하도록 로직 변경

--- src/main/java/com/konggogi/veganlife/sse/service/NotificationService.java
+++ src/main/java/com/konggogi/veganlife/sse/service/NotificationService.java
@@ -2,11 +2,16 @@ package com.konggogi.veganlife.sse.service;
 
 
 import com.konggogi.veganlife.global.exception.ErrorCode;
+import com.konggogi.veganlife.member.domain.Member;
 import com.konggogi.veganlife.member.service.MemberQueryService;
+import com.konggogi.veganlife.sse.domain.Notification;
 import com.konggogi.veganlife.sse.domain.NotificationMessage;
 import com.konggogi.veganlife.sse.domain.NotificationType;
+import com.konggogi.veganlife.sse.domain.mapper.NotificationMapper;
 import com.konggogi.veganlife.sse.exception.SseConnectionException;
 import com.konggogi.veganlife.sse.repository.EmitterRepository;
+import com.konggogi.veganlife.sse.repository.NotificationRepository;
+import com.konggogi.veganlife.sse.service.dto.NotificationData;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,27 +22,36 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 public class NotificationService {
     private static final Long DEFAULT_TIMEOUT = 60 * 1000L;
     private static final Long RECONNECTION_TIME = 1000L;
+    private final NotificationRepository notificationRepository;
     private final EmitterRepository emitterRepository;
-
     private final MemberQueryService memberQueryService;
+    private final NotificationMapper notificationMapper;
 
     public SseEmitter subscribe(Long memberId) {
-        memberQueryService.search(memberId);
+        Member member = memberQueryService.search(memberId);
         SseEmitter emitter = createEmitter(memberId);
-
-        sendToClient(memberId, NotificationType.SSE, NotificationMessage.SSE_CONNECTION);
+        sendNotification(
+                member, NotificationType.SSE, NotificationMessage.SSE_CONNECTION.getMessage());
         return emitter;
     }
 
-    private void sendToClient(Long memberId, NotificationType type, Object data) {
+    public void sendNotification(Member member, NotificationType type, String message) {
+        Notification notification =
+                notificationRepository.save(notificationMapper.toEntity(member, type, message));
+        NotificationData notificationData = notificationMapper.toNotificationData(notification);
+        sendToClient(member.getId(), notificationData);
+    }
+
+    private void sendToClient(Long memberId, Object data) {
         emitterRepository
                 .findById(memberId)
                 .ifPresent(
                         emitter -> {
                             try {
-                                emitter.send(createSseEvent(memberId, type, data));
+                                emitter.send(createSseEvent(memberId, data));
                             } catch (IOException exception) {
                                 emitterRepository.deleteById(memberId);
+                                // TODO 에러 핸들링
                                 throw new SseConnectionException(ErrorCode.SSE_CONNECTION_ERROR);
                             }
                         });
@@ -52,11 +66,9 @@ public class NotificationService {
         return emitter;
     }
 
-    private SseEmitter.SseEventBuilder createSseEvent(
-            Long memberId, NotificationType type, Object data) {
+    private SseEmitter.SseEventBuilder createSseEvent(Long memberId, Object data) {
         return SseEmitter.event()
                 .id(String.valueOf(memberId))
-                .name(type.name())
                 .data(data)
                 .reconnectTime(RECONNECTION_TIME);
     }
