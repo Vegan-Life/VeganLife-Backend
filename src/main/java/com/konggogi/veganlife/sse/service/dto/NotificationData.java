36d3aa0d1143ab1ca0a43ba13da1b9b236febb59
sojk401 <sojk401@gmail.com>
2023-12-29 22:45:29 +0900

    [#170] feat: createdAt 필드 추가

--- src/main/java/com/konggogi/veganlife/sse/service/dto/NotificationData.java
+++ src/main/java/com/konggogi/veganlife/sse/service/dto/NotificationData.java
@@ -2,5 +2,6 @@ package com.konggogi.veganlife.sse.service.dto;
 
 
 import com.konggogi.veganlife.sse.domain.NotificationType;
+import java.time.LocalDateTime;
 
-public record NotificationData(NotificationType type, String message) {}
+public record NotificationData(NotificationType type, String message, LocalDateTime createdAt) {}
