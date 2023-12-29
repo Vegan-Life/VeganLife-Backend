fe249ffb521f520ca97ab09f6a26a8d3cad85c1b
sojk401 <sojk401@gmail.com>
2023-12-29 20:17:43 +0900

    [#170] feat: findByDateAndType() 정의

--- src/main/java/com/konggogi/veganlife/sse/repository/NotificationRepository.java
+++ src/main/java/com/konggogi/veganlife/sse/repository/NotificationRepository.java
@@ -2,6 +2,15 @@ package com.konggogi.veganlife.sse.repository;
 
 
 import com.konggogi.veganlife.sse.domain.Notification;
+import com.konggogi.veganlife.sse.domain.NotificationType;
+import java.time.LocalDate;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface NotificationRepository extends JpaRepository<Notification, Long> {}
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    @Query(
+            "select n from Notification n where n.member.id = :memberId and cast(n.createdAt as localdate) = :date and n.type = :type")
+    Optional<Notification> findByDateAndType(Long memberId, LocalDate date, NotificationType type);
+}
