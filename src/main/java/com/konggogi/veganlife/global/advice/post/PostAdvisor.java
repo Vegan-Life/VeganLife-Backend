63dd8a6a3ddf5e64d635c2a7f9b0ab357baa6310
sojk401 <sojk401@gmail.com>
2024-01-01 01:24:07 +0900

    [#186] feat: IllegalCommentException 예외 핸들링

--- src/main/java/com/konggogi/veganlife/global/advice/post/PostAdvisor.java
+++ src/main/java/com/konggogi/veganlife/global/advice/post/PostAdvisor.java
@@ -1,6 +1,7 @@
 package com.konggogi.veganlife.global.advice.post;
 
 
+import com.konggogi.veganlife.comment.exception.IllegalCommentException;
 import com.konggogi.veganlife.global.exception.dto.response.ErrorResponse;
 import com.konggogi.veganlife.global.util.AopUtils;
 import com.konggogi.veganlife.global.util.LoggingUtils;
@@ -12,13 +13,22 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.HandlerMethod;
 
 @RestControllerAdvice
-public class PostControllerAdvice {
+public class PostAdvisor {
     @ExceptionHandler(IllegalLikeStatusException.class)
-    public ResponseEntity<ErrorResponse> handleDuplicateLikeExceptionException(
+    public ResponseEntity<ErrorResponse> handleIllegalLikeStatusException(
             HandlerMethod handlerMethod, IllegalLikeStatusException exception) {
         LoggingUtils.exceptionLog(
                 AopUtils.extractMethodSignature(handlerMethod), HttpStatus.CONFLICT, exception);
         return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(ErrorResponse.from(exception.getErrorCode()));
+    }
+
+    @ExceptionHandler(IllegalCommentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalCommentException(
+            HandlerMethod handlerMethod, IllegalCommentException exception) {
+        LoggingUtils.exceptionLog(
+                AopUtils.extractMethodSignature(handlerMethod), HttpStatus.BAD_REQUEST, exception);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ErrorResponse.from(exception.getErrorCode()));
     }
 }
