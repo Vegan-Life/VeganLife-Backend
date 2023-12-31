535606466b7384c7d3f4008ac69521dea917de59
sojk401 <sojk401@gmail.com>
2024-01-01 01:20:18 +0900

    [#186] feat: CommentMapper 생성

---
+++ src/main/java/com/konggogi/veganlife/comment/domain/mapper/CommentMapper.java
@@ -0,0 +1,18 @@
+package com.konggogi.veganlife.comment.domain.mapper;
+
+
+import com.konggogi.veganlife.comment.controller.dto.request.CommentAddRequest;
+import com.konggogi.veganlife.comment.controller.dto.response.CommentAddResponse;
+import com.konggogi.veganlife.comment.domain.Comment;
+import com.konggogi.veganlife.member.domain.Member;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+    @Mapping(target = "id", ignore = true)
+    Comment toEntity(Member member, CommentAddRequest commentAddRequest);
+
+    @Mapping(target = "commentId", source = "comment.id")
+    CommentAddResponse toCommentAddResponse(Comment comment);
+}
