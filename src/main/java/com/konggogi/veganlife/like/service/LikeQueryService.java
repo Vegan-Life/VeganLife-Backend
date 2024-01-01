0c9a5571cb7f678002f37a23f4cada5f51fa9bde
sojk401 <sojk401@gmail.com>
2024-01-02 00:14:37 +0900

    [#188] feat: searchCommentLike() 추가

--- src/main/java/com/konggogi/veganlife/like/service/LikeQueryService.java
+++ src/main/java/com/konggogi/veganlife/like/service/LikeQueryService.java
@@ -1,7 +1,9 @@
 package com.konggogi.veganlife.like.service;
 
 
+import com.konggogi.veganlife.like.domain.CommentLike;
 import com.konggogi.veganlife.like.domain.PostLike;
+import com.konggogi.veganlife.like.repository.CommentLikeRepository;
 import com.konggogi.veganlife.like.repository.PostLikeRepository;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -13,8 +15,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class LikeQueryService {
     private final PostLikeRepository postLikeRepository;
+    private final CommentLikeRepository commentLikeRepository;
 
     public Optional<PostLike> searchPostLike(Long memberId, Long postId) {
         return postLikeRepository.findByMemberIdAndId(memberId, postId);
+    }
+
+    public Optional<CommentLike> searchCommentLike(Long memberId, Long commentId) {
+        return commentLikeRepository.findByMemberIdAndId(memberId, commentId);
     }
 }
