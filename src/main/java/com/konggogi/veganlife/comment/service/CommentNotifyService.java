051c9beed7ba78e86fc3ae69c32f86aae5b7c810
sojk401 <sojk401@gmail.com>
2024-01-03 03:02:08 +0900

    [#194] feat: 댓글 등록시 알림 호출

---
+++ src/main/java/com/konggogi/veganlife/comment/service/CommentNotifyService.java
@@ -0,0 +1,36 @@
+package com.konggogi.veganlife.comment.service;
+
+
+import com.konggogi.veganlife.member.domain.Member;
+import com.konggogi.veganlife.member.service.MemberQueryService;
+import com.konggogi.veganlife.notification.domain.NotificationMessage;
+import com.konggogi.veganlife.notification.domain.NotificationType;
+import com.konggogi.veganlife.notification.service.NotificationService;
+import com.konggogi.veganlife.post.service.PostQueryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
+public class CommentNotifyService {
+    private final MemberQueryService memberQueryService;
+    private final PostQueryService postQueryService;
+    private final NotificationService notificationService;
+
+    public void notifyAddCommentIfNotPostOwner(Long authorId, Long postId) {
+        Member commentAuthor = memberQueryService.search(authorId);
+        Member postAuthor = postQueryService.search(postId).getMember();
+        if (commentAuthor.equals(postAuthor)) {
+            return;
+        }
+        notificationService.sendNotification(
+                postAuthor,
+                NotificationType.COMMENT,
+                NotificationMessage.ADD_COMMENT.getMessage(
+                        commentAuthor.getNickname(), postAuthor.getNickname()));
+    }
+}
