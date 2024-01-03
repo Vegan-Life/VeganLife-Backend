8a330e4a7120b6477a7b896d9b6a2b5d9c31d728
sypark <soun997@gmail.com>
2024-01-03 16:04:35 +0900

    [#177] fix: MealLog 조회 시 fetch join, batch_fetch_size 적용

--- src/main/java/com/konggogi/veganlife/meallog/repository/MealLogRepository.java
+++ src/main/java/com/konggogi/veganlife/meallog/repository/MealLogRepository.java
@@ -5,6 +5,7 @@ import com.konggogi.veganlife.meallog.domain.MealLog;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -14,7 +15,11 @@ public interface MealLogRepository extends JpaRepository<MealLog, Long> {
     List<MealLog> findAllByMemberIdAndCreatedAtBetween(
             Long memberId, LocalDateTime startDate, LocalDateTime endDate);
 
+    @Query(" select distinct m from MealLog m join fetch m.meals" + " where m.id = :id")
+    Optional<MealLog> findById(Long id);
+
     @Query(
-            "select m from MealLog m where cast(m.createdAt as localdate) = :date and m.member.id = :memberId")
+            " select distinct m from MealLog m join fetch m.meals"
+                    + " where cast(m.createdAt as localdate) = :date and m.member.id = :memberId")
     List<MealLog> findAllByDate(LocalDate date, Long memberId);
 }
