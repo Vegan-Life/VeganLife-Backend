9ca21361d4b55b58245052cbc272c164afa114d9
sypark <soun997@gmail.com>
2024-01-03 16:53:41 +0900

    [#177] fix: MealLog의 Member 연관관계 fetch 전략을 LAZY로 수정

--- src/main/java/com/konggogi/veganlife/meallog/domain/MealLog.java
+++ src/main/java/com/konggogi/veganlife/meallog/domain/MealLog.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -41,7 +42,7 @@ public class MealLog extends TimeStamped {
     @OneToMany(mappedBy = "mealLog", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<MealImage> mealImages = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
