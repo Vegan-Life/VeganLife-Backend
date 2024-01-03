e637ccb26ba898691c4706e58497d5e1d5e466d4
sojk401 <sojk401@gmail.com>
2024-01-04 05:04:19 +0900

    [#201] fix: findById -> findByIdFetchJoinMember 메서드명 변경

--- src/main/java/com/konggogi/veganlife/comment/repository/CommentRepository.java
+++ src/main/java/com/konggogi/veganlife/comment/repository/CommentRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Query("select distinct c from Comment c join fetch c.member " + "where c.id = :commentId")
-    Optional<Comment> findById(Long commentId);
+    Optional<Comment> findByIdFetchJoinMember(Long commentId);
 
     @Modifying(clearAutomatically = true)
     @Query("update Comment c set c.member = null where c.member.id = :memberId")
