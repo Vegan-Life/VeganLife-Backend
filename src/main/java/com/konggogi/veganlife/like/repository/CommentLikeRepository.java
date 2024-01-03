e863f28eb201abf77baa80575c6b8d928e8a2945
sojk401 <sojk401@gmail.com>
2024-01-03 19:00:25 +0900

    [#129] fix: setMemberToNull 메서드 정의

--- src/main/java/com/konggogi/veganlife/like/repository/CommentLikeRepository.java
+++ src/main/java/com/konggogi/veganlife/like/repository/CommentLikeRepository.java
@@ -4,7 +4,13 @@ package com.konggogi.veganlife.like.repository;
 import com.konggogi.veganlife.like.domain.CommentLike;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
     Optional<CommentLike> findByMemberIdAndCommentId(Long memberId, Long commentId);
+
+    @Modifying
+    @Query("UPDATE CommentLike c SET c.member = NULL WHERE c.member.id = :memberId")
+    void setMemberToNull(Long memberId);
 }
