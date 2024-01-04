86556abc19ac8c15c75fc553950eb0e0a046b3bb
sojk401 <sojk401@gmail.com>
2024-01-05 02:17:32 +0900

    [#208] feat: 게시글 상세 조회 API 구현

--- src/main/java/com/konggogi/veganlife/post/controller/PostController.java
+++ src/main/java/com/konggogi/veganlife/post/controller/PostController.java
@@ -4,10 +4,13 @@ package com.konggogi.veganlife.post.controller;
 import com.konggogi.veganlife.global.security.user.UserDetailsImpl;
 import com.konggogi.veganlife.post.controller.dto.request.PostAddRequest;
 import com.konggogi.veganlife.post.controller.dto.response.PostAddResponse;
+import com.konggogi.veganlife.post.controller.dto.response.PostDetailsResponse;
 import com.konggogi.veganlife.post.domain.Post;
 import com.konggogi.veganlife.post.domain.mapper.PostMapper;
 import com.konggogi.veganlife.post.service.PostLikeService;
+import com.konggogi.veganlife.post.service.PostSearchService;
 import com.konggogi.veganlife.post.service.PostService;
+import com.konggogi.veganlife.post.service.dto.PostDetailsDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,6 +23,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/v1/posts")
 public class PostController {
     private final PostService postService;
+    private final PostSearchService postSearchService;
     private final PostLikeService postLikeService;
     private final PostMapper postMapper;
 
@@ -29,6 +33,14 @@ public class PostController {
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
         Post post = postService.add(userDetails.id(), postAddRequest);
         return ResponseEntity.ok(postMapper.toPostAddResponse(post));
+    }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<PostDetailsResponse> getPost(
+            @PathVariable Long postId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        PostDetailsDto postDetailsDto =
+                postSearchService.searchDetailsById(userDetails.id(), postId);
+        return ResponseEntity.ok(postMapper.toPostDetailsResponse(postDetailsDto));
     }
 
     @PostMapping("/{postId}/likes")
