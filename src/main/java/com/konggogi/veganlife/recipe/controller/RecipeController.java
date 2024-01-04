b9a5c9d7d99adff231a0176e93c1b89c91907bb4
sypark <soun997@gmail.com>
2024-01-05 02:52:19 +0900

    [#198] feat: Recipe 목록 조회 controller 구현

---
+++ src/main/java/com/konggogi/veganlife/recipe/controller/RecipeController.java
@@ -0,0 +1,29 @@
+package com.konggogi.veganlife.recipe.controller;
+
+
+import com.konggogi.veganlife.member.domain.VegetarianType;
+import com.konggogi.veganlife.recipe.controller.dto.response.RecipeListResponse;
+import com.konggogi.veganlife.recipe.service.RecipeSearchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/recipes")
+public class RecipeController {
+
+    private final RecipeSearchService recipeSearchService;
+
+    @GetMapping
+    public ResponseEntity<Page<RecipeListResponse>> getRecipeList(
+            VegetarianType vegetarianType, Pageable pageable) {
+
+        Page<RecipeListResponse> page = recipeSearchService.searchAll(vegetarianType, pageable);
+        return ResponseEntity.ok(page);
+    }
+}
