b9c47e6f6c2aa8fa46267b391392a8b6f4dce243
sojk401 <sojk401@gmail.com>
2024-01-04 23:47:38 +0900

    [#206] refactor: LikeMapper를 PostLike/CommentLike Mapper로 분리

--- src/main/java/com/konggogi/veganlife/comment/service/CommentLikeService.java
+++ src/main/java/com/konggogi/veganlife/comment/service/CommentLikeService.java
@@ -3,9 +3,9 @@ package com.konggogi.veganlife.comment.service;
 
 import com.konggogi.veganlife.comment.domain.Comment;
 import com.konggogi.veganlife.comment.domain.CommentLike;
+import com.konggogi.veganlife.comment.domain.mapper.CommentLikeMapper;
 import com.konggogi.veganlife.comment.repository.CommentLikeRepository;
 import com.konggogi.veganlife.global.exception.ErrorCode;
-import com.konggogi.veganlife.like.domain.mapper.LikeMapper;
 import com.konggogi.veganlife.member.domain.Member;
 import com.konggogi.veganlife.member.service.MemberQueryService;
 import com.konggogi.veganlife.post.domain.Post;
@@ -25,14 +25,14 @@ public class CommentLikeService {
     private final CommentLikeQueryService commentLikeQueryService;
     private final CommentLikeNotifyService commentLikeNotifyService;
     private final CommentLikeRepository commentLikeRepository;
-    private final LikeMapper likeMapper;
+    private final CommentLikeMapper commentLikeMapper;
 
     public void addCommentLike(Long memberId, Long postId, Long commentId) {
         Member member = memberQueryService.search(memberId);
         Post post = postQueryService.search(postId);
         Comment comment = commentQueryService.search(commentId);
         validateCommentLikeIsExist(memberId, commentId);
-        CommentLike commentLike = likeMapper.toCommentLike(member, post);
+        CommentLike commentLike = commentLikeMapper.toCommentLike(member, post);
         comment.addCommentLike(commentLike);
         commentLikeNotifyService.notifyAddCommentLikeIfNotAuthor(memberId, commentId);
     }
--- src/main/java/com/konggogi/veganlife/post/service/PostLikeService.java
+++ src/main/java/com/konggogi/veganlife/post/service/PostLikeService.java
@@ -2,11 +2,11 @@ package com.konggogi.veganlife.post.service;
 
 
 import com.konggogi.veganlife.global.exception.ErrorCode;
-import com.konggogi.veganlife.like.domain.mapper.LikeMapper;
 import com.konggogi.veganlife.member.domain.Member;
 import com.konggogi.veganlife.member.service.MemberQueryService;
 import com.konggogi.veganlife.post.domain.Post;
 import com.konggogi.veganlife.post.domain.PostLike;
+import com.konggogi.veganlife.post.domain.mapper.PostLikeMapper;
 import com.konggogi.veganlife.post.exception.IllegalLikeStatusException;
 import com.konggogi.veganlife.post.repository.PostLikeRepository;
 import lombok.RequiredArgsConstructor;
@@ -21,13 +21,13 @@ public class PostLikeService {
     private final PostQueryService postQueryService;
     private final PostLikeQueryService likeQueryService;
     private final PostLikeRepository postLikeRepository;
-    private final LikeMapper likeMapper;
+    private final PostLikeMapper postLikeMapper;
 
     public void addPostLike(Long memberId, Long postId) {
         Member member = memberQueryService.search(memberId);
         Post post = postQueryService.search(postId);
         validatePostLikeIsExist(memberId, postId);
-        PostLike postLike = likeMapper.toPostLike(member);
+        PostLike postLike = postLikeMapper.toPostLike(member);
         post.addPostLike(postLike);
     }
 
