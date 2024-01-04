b8c1ac1c365a2a8be6f198991c22d956d2a7e03b
sojk401 <sojk401@gmail.com>
2024-01-05 07:30:10 +0900

    [#212] feat: getThumbnailUrl() 구현

--- src/main/java/com/konggogi/veganlife/post/domain/Post.java
+++ src/main/java/com/konggogi/veganlife/post/domain/Post.java
@@ -82,4 +82,11 @@ public class Post extends TimeStamped {
     public int countComments() {
         return comments.size();
     }
+
+    public String getThumbnailUrl() {
+        if (imageUrls.isEmpty()) {
+            return null;
+        }
+        return imageUrls.get(0).getImageUrl();
+    }
 }
