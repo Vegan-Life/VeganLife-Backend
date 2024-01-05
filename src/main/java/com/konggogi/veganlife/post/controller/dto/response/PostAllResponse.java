413f1364c9152185f2d50f80ba5c3c07e8a02ec4
sojk401 <sojk401@gmail.com>
2024-01-05 13:19:11 +0900

    [#212] feat: PostSimpleResponse 생성

--- src/main/java/com/konggogi/veganlife/post/controller/dto/response/PostAllResponse.java
+++
@@ -1,8 +0,0 @@
-package com.konggogi.veganlife.post.controller.dto.response;
-
-
-import com.konggogi.veganlife.post.service.dto.PostSimpleDto;
-import java.util.List;
-import org.springframework.data.domain.Page;
-
-public record PostAllResponse(List<String> topTags, Page<PostSimpleDto> posts) {}
