7102e384048dc0e04e24bfd2196f704e3a52e19a
sypark <soun997@gmail.com>
2024-01-10 13:30:39 +0900

    [#211] feat: Recipe 상세조회 service 구현

--- src/main/java/com/konggogi/veganlife/recipe/service/RecipeQueryService.java
+++ src/main/java/com/konggogi/veganlife/recipe/service/RecipeQueryService.java
@@ -1,6 +1,8 @@
 package com.konggogi.veganlife.recipe.service;
 
 
+import com.konggogi.veganlife.global.exception.ErrorCode;
+import com.konggogi.veganlife.global.exception.NotFoundEntityException;
 import com.konggogi.veganlife.member.domain.VegetarianType;
 import com.konggogi.veganlife.recipe.domain.Recipe;
 import com.konggogi.veganlife.recipe.repository.RecipeRepository;
@@ -20,5 +22,11 @@ public class RecipeQueryService {
     public Page<Recipe> searchAllByRecipeType(VegetarianType vegetarianType, Pageable pageable) {
 
         return recipeRepository.findAllByRecipeTypes(vegetarianType, pageable);
+    }
+
+    public Recipe search(Long id) {
+
+        return recipeRepository.findById(id)
+                .orElseThrow(() -> new NotFoundEntityException(ErrorCode.NOT_FOUND_RECIPE));
     }
 }
