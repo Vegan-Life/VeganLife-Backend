aaff29b63b2f7ed9c018c1bb73267556a6c174f5
sypark <soun997@gmail.com>
2024-01-10 14:27:11 +0900

    [#211] fix: Recipe Ingredient, Description Fixture 커스터마이징 메서드 추가

--- src/test/java/com/konggogi/veganlife/recipe/fixture/RecipeIngredientFixture.java
+++ src/test/java/com/konggogi/veganlife/recipe/fixture/RecipeIngredientFixture.java
@@ -3,12 +3,12 @@ package com.konggogi.veganlife.recipe.fixture;
 
 import com.konggogi.veganlife.recipe.domain.RecipeIngredient;
 
-public enum RecipeIngredientsFixture {
+public enum RecipeIngredientFixture {
     DEFAULT("올리브유 1 tbsp");
 
     private String name;
 
-    RecipeIngredientsFixture(String name) {
+    RecipeIngredientFixture(String name) {
         this.name = name;
     }
 
@@ -20,5 +20,10 @@ public enum RecipeIngredientsFixture {
     public RecipeIngredient get(Long id) {
 
         return RecipeIngredient.builder().id(id).name(name).build();
+    }
+
+    public RecipeIngredient getWithName(String name) {
+
+        return RecipeIngredient.builder().name(name).build();
     }
 }
