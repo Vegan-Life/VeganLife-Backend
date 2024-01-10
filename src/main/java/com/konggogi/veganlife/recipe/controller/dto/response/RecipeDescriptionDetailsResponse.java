fa992164e7aa5cc5d28477ebb41e0e17015ab376
sypark <soun997@gmail.com>
2024-01-10 14:19:55 +0900

    [#211] fix: RecipeDetails response dto 필드 수정

--- src/main/java/com/konggogi/veganlife/recipe/controller/dto/response/RecipeDescriptionDetailsResponse.java
+++
@@ -1,4 +0,0 @@
-package com.konggogi.veganlife.recipe.controller.dto.response;
-
-public record RecipeDescriptionDetailsResponse(Integer sequence,
-                                               String description) {}
