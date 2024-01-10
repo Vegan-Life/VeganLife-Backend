c9f096f73d2ba1ac55015aa1bfd52502e43a5bb7
sypark <soun997@gmail.com>
2024-01-10 13:30:58 +0900

    [#211] feat: Recipe -> RecipeDetails mapping 메서드 구현

--- src/main/java/com/konggogi/veganlife/recipe/domain/mapper/RecipeMapper.java
+++ src/main/java/com/konggogi/veganlife/recipe/domain/mapper/RecipeMapper.java
@@ -2,9 +2,13 @@ package com.konggogi.veganlife.recipe.domain.mapper;
 
 
 import com.konggogi.veganlife.member.domain.VegetarianType;
+import com.konggogi.veganlife.recipe.controller.dto.response.RecipeDescriptionDetailsResponse;
+import com.konggogi.veganlife.recipe.controller.dto.response.RecipeDetailsResponse;
 import com.konggogi.veganlife.recipe.controller.dto.response.RecipeListResponse;
 import com.konggogi.veganlife.recipe.domain.Recipe;
+import com.konggogi.veganlife.recipe.domain.RecipeDescription;
 import com.konggogi.veganlife.recipe.domain.RecipeImage;
+import com.konggogi.veganlife.recipe.domain.RecipeIngredient;
 import com.konggogi.veganlife.recipe.domain.RecipeType;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -14,14 +18,25 @@ import org.mapstruct.Named;
 public interface RecipeMapper {
 
     @Mapping(
+            source = "recipe.thumbnailUrl",
             target = "thumbnailUrl",
-            expression = "java(RecipeMapper.recipeImageToImageUrl(recipe.getThumbnailUrl()))")
+            qualifiedByName = "recipeImageToImageUrl")
     @Mapping(
             source = "recipe.recipeTypes",
             target = "recipeTypes",
             qualifiedByName = "recipeTypeToVegetarianType")
     RecipeListResponse toRecipeListResponse(Recipe recipe);
 
+
+    @Mapping(
+            source = "recipe.recipeTypes",
+            target = "recipeTypes",
+            qualifiedByName = "recipeTypeToVegetarianType")
+    @Mapping(source = "recipe.recipeImages", target = "imageUrls", qualifiedByName = "recipeImageToImageUrl")
+    @Mapping(source = "recipe.ingredients", target = "ingredients", qualifiedByName = "recipeIngredientsToString")
+    RecipeDetailsResponse toRecipeDetailsResponse(Recipe recipe, boolean isLiked);
+
+    @Named("recipeImageToImageUrl")
     static String recipeImageToImageUrl(RecipeImage recipeImage) {
 
         return recipeImage.getImageUrl();
@@ -31,5 +46,11 @@ public interface RecipeMapper {
     static VegetarianType recipeTypeToVegetarianType(RecipeType recipeType) {
 
         return recipeType.getVegetarianType();
+    }
+
+    @Named("recipeIngredientsToString")
+    static String recipeIngredientsToString(RecipeIngredient ingredient) {
+
+        return ingredient.getName();
     }
 }
