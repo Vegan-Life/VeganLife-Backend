3547ffd2850b0facc2a156012812cee14c312bc6
sypark <soun997@gmail.com>
2024-01-10 13:51:45 +0900

    [#211] test: RecipeIngredients 엔티티 필드 수정에 따른 Fixture 수정

--- src/test/java/com/konggogi/veganlife/recipe/fixture/RecipeIngredientsFixture.java
+++ src/test/java/com/konggogi/veganlife/recipe/fixture/RecipeIngredientsFixture.java
@@ -4,25 +4,21 @@ package com.konggogi.veganlife.recipe.fixture;
 import com.konggogi.veganlife.recipe.domain.RecipeIngredient;
 
 public enum RecipeIngredientsFixture {
-    DEFAULT("올리브유", 1, "tbsp");
+    DEFAULT("올리브유 1 tbsp");
 
     private String name;
-    private Integer amount;
-    private String unit;
 
-    RecipeIngredientsFixture(String name, Integer amount, String unit) {
+    RecipeIngredientsFixture(String name) {
         this.name = name;
-        this.amount = amount;
-        this.unit = unit;
     }
 
     public RecipeIngredient get() {
 
-        return RecipeIngredient.builder().name(name).amount(amount).unit(unit).build();
+        return RecipeIngredient.builder().name(name).build();
     }
 
     public RecipeIngredient get(Long id) {
 
-        return RecipeIngredient.builder().id(id).name(name).amount(amount).unit(unit).build();
+        return RecipeIngredient.builder().id(id).name(name).build();
     }
 }
