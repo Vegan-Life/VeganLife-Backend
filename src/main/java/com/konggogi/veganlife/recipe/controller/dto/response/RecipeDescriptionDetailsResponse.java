e663aa4d19b5ce6a9419e4f729bb259b910ee12b
sypark <soun997@gmail.com>
2024-01-10 13:29:44 +0900

    [#211] feat: RecipeDetails response dto 구현

---
+++ src/main/java/com/konggogi/veganlife/recipe/controller/dto/response/RecipeDescriptionDetailsResponse.java
@@ -0,0 +1,4 @@
+package com.konggogi.veganlife.recipe.controller.dto.response;
+
+public record RecipeDescriptionDetailsResponse(Integer sequence,
+                                               String description) {}
