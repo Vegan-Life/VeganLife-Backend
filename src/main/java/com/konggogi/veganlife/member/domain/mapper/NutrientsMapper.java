39652ca9dc6d5f1aec44e470a2b6afcc325b6ee4
sojk401 <sojk401@gmail.com>
2024-01-11 23:32:25 +0900

    [#235] refactor: MemberMapper의 toRecommendNutrientsResponse 메서드를 NutrientsMapper로 이동

--- src/main/java/com/konggogi/veganlife/member/domain/mapper/NutrientsMapper.java
+++ src/main/java/com/konggogi/veganlife/member/domain/mapper/NutrientsMapper.java
@@ -2,7 +2,9 @@ package com.konggogi.veganlife.member.domain.mapper;
 
 
 import com.konggogi.veganlife.member.controller.dto.response.CalorieIntakeResponse;
+import com.konggogi.veganlife.member.controller.dto.response.RecommendNutrientsResponse;
 import com.konggogi.veganlife.member.controller.dto.response.TodayIntakeResponse;
+import com.konggogi.veganlife.member.domain.Member;
 import com.konggogi.veganlife.member.service.dto.CaloriesOfMealType;
 import com.konggogi.veganlife.member.service.dto.IntakeNutrients;
 import java.util.List;
@@ -12,6 +14,9 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface NutrientsMapper {
     TodayIntakeResponse toTodayIntakeResponse(IntakeNutrients intakeNutrients);
+
+    @Mapping(target = "dailyCalorie", source = "member.AMR")
+    RecommendNutrientsResponse toRecommendNutrientsResponse(Member member);
 
     @Mapping(source = "caloriesOfMealTypes", target = "periodicCalorie")
     CalorieIntakeResponse toCalorieIntakeResponse(
