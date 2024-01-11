ce767803d0dda85cbc6afde1f7b2c50a98688ca9
sojk401 <sojk401@gmail.com>
2024-01-11 23:24:31 +0900

    [#235] refactor: MyPageController를 생성하여 마이페이지 관련 API 이동

--- src/main/java/com/konggogi/veganlife/member/controller/MemberController.java
+++ src/main/java/com/konggogi/veganlife/member/controller/MemberController.java
@@ -3,7 +3,6 @@ package com.konggogi.veganlife.member.controller;
 
 import com.konggogi.veganlife.global.security.user.UserDetailsImpl;
 import com.konggogi.veganlife.member.controller.dto.request.MemberInfoRequest;
-import com.konggogi.veganlife.member.controller.dto.request.MemberProfileRequest;
 import com.konggogi.veganlife.member.controller.dto.response.*;
 import com.konggogi.veganlife.member.domain.Member;
 import com.konggogi.veganlife.member.domain.mapper.MemberMapper;
@@ -13,15 +12,10 @@ import com.konggogi.veganlife.member.service.MemberService;
 import com.konggogi.veganlife.member.service.NutrientsQueryService;
 import com.konggogi.veganlife.member.service.dto.CaloriesOfMealType;
 import com.konggogi.veganlife.member.service.dto.IntakeNutrients;
-import com.konggogi.veganlife.post.controller.dto.response.PostSimpleResponse;
-import com.konggogi.veganlife.post.domain.mapper.PostMapper;
-import com.konggogi.veganlife.post.service.PostSearchService;
 import jakarta.validation.Valid;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -34,10 +28,8 @@ public class MemberController {
     private final MemberService memberService;
     private final MemberQueryService memberQueryService;
     private final NutrientsQueryService nutrientsQueryService;
-    private final PostSearchService postSearchService;
     private final MemberMapper memberMapper;
     private final NutrientsMapper nutrientsMapper;
-    private final PostMapper postMapper;
 
     @PostMapping()
     public ResponseEntity<MemberInfoResponse> modifyMemberInfo(
@@ -51,21 +43,6 @@ public class MemberController {
     public ResponseEntity<Void> removeMember(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         memberService.removeMember(userDetails.id());
         return ResponseEntity.noContent().build();
-    }
-
-    @GetMapping("/profile")
-    public ResponseEntity<MemberProfileResponse> getMemberDetails(
-            @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        Member member = memberQueryService.search(userDetails.id());
-        return ResponseEntity.ok(memberMapper.toMemberProfileResponse(member));
-    }
-
-    @PutMapping("/profile")
-    public ResponseEntity<MemberProfileResponse> modifyMemberProfile(
-            @AuthenticationPrincipal UserDetailsImpl userDetails,
-            @RequestBody @Valid MemberProfileRequest memberProfileRequest) {
-        Member member = memberService.modifyMemberProfile(userDetails.id(), memberProfileRequest);
-        return ResponseEntity.ok(memberMapper.toMemberProfileResponse(member));
     }
 
     @GetMapping("/nutrients")
@@ -119,25 +96,5 @@ public class MemberController {
         int totalCalorie = nutrientsQueryService.calcTotalCalorie(mealCalories);
         return ResponseEntity.ok(
                 nutrientsMapper.toCalorieIntakeResponse(totalCalorie, mealCalories));
-    }
-
-    @GetMapping("/me/posts")
-    public ResponseEntity<Page<PostSimpleResponse>> getMyPostList(
-            Pageable pageable, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        Page<PostSimpleResponse> postSimpleResponses =
-                postSearchService
-                        .searchAllSimple(userDetails.id(), pageable)
-                        .map(postMapper::toPostSimpleResponse);
-        return ResponseEntity.ok(postSimpleResponses);
-    }
-
-    @GetMapping("/me/posts-with-comments")
-    public ResponseEntity<Page<PostSimpleResponse>> getPostListContainingMyComment(
-            Pageable pageable, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        Page<PostSimpleResponse> postSimpleResponses =
-                postSearchService
-                        .searchByMemberComments(userDetails.id(), pageable)
-                        .map(postMapper::toPostSimpleResponse);
-        return ResponseEntity.ok(postSimpleResponses);
     }
 }
