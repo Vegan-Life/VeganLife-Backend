0dde34011efb567241b0b748cd25620b4081ac6b
sypark <soun997@gmail.com>
2024-01-11 14:04:42 +0900

    [#222] fix: 누락된 @RequestBody 추가

--- src/main/java/com/konggogi/veganlife/recipe/controller/RecipeController.java
+++ src/main/java/com/konggogi/veganlife/recipe/controller/RecipeController.java
@@ -12,11 +12,13 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,10 +45,11 @@ public class RecipeController {
 
     @PostMapping
     public ResponseEntity<Void> addRecipe(
-            @Valid RecipeAddRequest request, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+            @Valid @RequestBody RecipeAddRequest request,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
         recipeService.add(request, userDetails.id());
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
