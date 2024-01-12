7d442757ff44f7d8caddaf11a58ccd9617b2b45a
sojk401 <sojk401@gmail.com>
2024-01-13 02:50:11 +0900

    [#244] refactor: 액세스 토큰 재발급 로직 RefreshTokenService로 코드 이동 및 구조 변경

--- src/main/java/com/konggogi/veganlife/member/controller/AuthController.java
+++ src/main/java/com/konggogi/veganlife/member/controller/AuthController.java
@@ -1,13 +1,10 @@
 package com.konggogi.veganlife.member.controller;
 
 
-import com.konggogi.veganlife.global.exception.ErrorCode;
-import com.konggogi.veganlife.global.security.exception.InvalidJwtException;
-import com.konggogi.veganlife.global.util.JwtUtils;
 import com.konggogi.veganlife.member.controller.dto.request.ReissueRequest;
 import com.konggogi.veganlife.member.controller.dto.response.AuthResponse;
 import com.konggogi.veganlife.member.domain.mapper.AuthMapper;
-import com.konggogi.veganlife.member.service.MemberQueryService;
+import com.konggogi.veganlife.member.service.RefreshTokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,20 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("api/v1/auth")
 public class AuthController {
-    private final MemberQueryService memberQueryService;
-    private final JwtUtils jwtUtils;
+    private final RefreshTokenService refreshTokenService;
     private final AuthMapper authMapper;
 
     @PostMapping("/reissue")
     public ResponseEntity<AuthResponse> reissueToken(@RequestBody ReissueRequest reissueRequest) {
         String refreshToken = reissueRequest.refreshToken();
-        String accessToken =
-                jwtUtils.extractBearerToken(refreshToken)
-                        .map(memberQueryService::reissueToken)
-                        .orElseThrow(
-                                () -> {
-                                    throw new InvalidJwtException(ErrorCode.INVALID_TOKEN);
-                                });
+        String accessToken = refreshTokenService.reissueAccessToken(refreshToken);
         return ResponseEntity.ok(authMapper.toAuthResponse(accessToken));
     }
 }
