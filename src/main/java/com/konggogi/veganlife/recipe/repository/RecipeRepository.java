d61723dcd89a04258889983e41f95361b6651a98
sypark <soun997@gmail.com>
2024-01-17 10:42:34 +0900

    [#223] feat: VegetarianType에 해당하는 Recipe 레코드 개수 조회 기능 구현

--- src/main/java/com/konggogi/veganlife/recipe/repository/RecipeRepository.java
+++ src/main/java/com/konggogi/veganlife/recipe/repository/RecipeRepository.java
@@ -3,7 +3,6 @@ package com.konggogi.veganlife.recipe.repository;
 
 import com.konggogi.veganlife.member.domain.VegetarianType;
 import com.konggogi.veganlife.recipe.domain.Recipe;
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,6 +12,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
+    // TODO: in vs exists 성능 비교
     /** parameter로 들어온 vegeterianType이 null이라면 전체 레코드를 조회한다. */
     @Query(
             "select r from Recipe r where exists"
@@ -21,11 +21,7 @@ public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     Page<Recipe> findAllByRecipeTypes(VegetarianType vegetarianType, Pageable pageable);
 
     @Query(
-            value =
-                    " select * from recipe"
-                            + " where recipe_id"
-                            + " in (select recipe_id from recipe_type where vegetarian_type = :#{#vegetarianType.name()})"
-                            + " order by rand() limit 4",
-            nativeQuery = true)
-    List<Recipe> findAllRandomByVegetarianType(VegetarianType vegetarianType);
+            " select count(r) from Recipe r"
+                    + " where exists (select t from r.recipeTypes t where t.vegetarianType = :vegetarianType)")
+    int countByRecipeType(VegetarianType vegetarianType);
 }
